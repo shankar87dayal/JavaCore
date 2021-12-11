b9729b05c36e34123ffa8d35843715e4252c4b96
User <User@DESKTOP-98P96A5>
2021-12-11 21:47:55 +0530

    Lab01

---
+++ src/edu/javacore/pract/Lab01.java
@@ -0,0 +1,16 @@
+package edu.javacore.pract;
+
+public class Lab01 {
+
+	public static void main(String[] args) {
+		Hello h1 = new Hello();
+		int aValue = h1.a;
+		System.out.println("Value of a is " + aValue);
+	}
+
+}
+
+class Hello {
+	int a;
+
+}
