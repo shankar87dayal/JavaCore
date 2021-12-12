664ff4ea513ea722dd6893bb248adca72efb1368
User <User@DESKTOP-98P96A5>
2021-12-12 16:56:50 +0530

    Lab16

---
+++ src/edu/javacore/pract/Lab16.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab16 {
+public static void main(String[] args) {
+   int  a= b= c= 99;
+    System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+}
+}
