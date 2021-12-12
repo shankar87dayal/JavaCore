16551aa07503b7ce820035551700756c11c1027b
User <User@DESKTOP-98P96A5>
2021-12-12 16:27:26 +0530

    Lab09

---
+++ src/edu/javacore/pract/Lab09.java
@@ -0,0 +1,14 @@
+package edu.javacore.pract;
+
+public class Lab09 {
+public static void main(String[] args) {
+		int a = 88;
+		boolean b = true;
+		double d = 9999.99;
+		String str = "raushan";
+		System.out.println("value of a is " + a);
+		System.out.println("value of b is " + b);
+		System.out.println("value of d is " + d);
+		System.out.println("value of str is " + str);
+	}
+}
