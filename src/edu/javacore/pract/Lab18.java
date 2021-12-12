4c4834d2d36a60e1b5b9436e5732869b1a1e8f63
User <User@DESKTOP-98P96A5>
2021-12-12 16:57:36 +0530

    Lab18

---
+++ src/edu/javacore/pract/Lab18.java
@@ -0,0 +1,11 @@
+package edu.javacore.pract;
+
+public class Lab18 {
+public static void main(String[] args) {
+	int a, b;
+    int  c = 10, a = 20, b = 30;
+    System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+}
+}
