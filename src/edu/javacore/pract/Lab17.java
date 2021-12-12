4909457b972fd1ebf6fbb6e217c7413dfeab163a
User <User@DESKTOP-98P96A5>
2021-12-12 16:57:09 +0530

    Lab17

---
+++ src/edu/javacore/pract/Lab17.java
@@ -0,0 +1,11 @@
+package edu.javacore.pract;
+
+public class Lab17 {
+public static void main(String[] args) {
+	int  b, c;
+    int a= b= c = 99;
+    System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+}
+}
