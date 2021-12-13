c2c86970c3c701171cdb82ce8f4f2b635779f04a
User <User@DESKTOP-98P96A5>
2021-12-13 12:34:00 +0530

    Lab22 program

---
+++ src/edu/javacore/pract/Lab22.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab22 {
+public static void main(String[] args) {
+		final int A = 99;
+		System.out.println(A);
+		A = 99;
+		System.out.println(A);
+}
+}
