b96caee0cb6e9b9b948c4139ccb57611327f6e33
User <User@DESKTOP-98P96A5>
2021-12-13 13:25:17 +0530

    Lab31 program

---
+++ src/edu/javacore/pract/Lab31.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab31 {
+public static void main(String[] args) {
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println(b1);
+		System.out.println(b2);
+}
+}
