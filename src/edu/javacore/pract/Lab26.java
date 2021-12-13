3bea21f3ea6f4f91e6c80f4d0110cc5653e1b52b
User <User@DESKTOP-98P96A5>
2021-12-13 13:08:25 +0530

    Lab26 program

---
+++ src/edu/javacore/pract/Lab26.java
@@ -0,0 +1,11 @@
+package edu.javacore.pract;
+
+public class Lab26 {
+public static void main(String[] args) {
+		final int A;
+		A = 99;
+		System.out.println(A);
+		A = 90;
+		System.out.println(A);
+}
+}
