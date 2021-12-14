c6cd4983ed5524367ebbfc97f7163b1f83929f1a
User <User@DESKTOP-98P96A5>
2021-12-14 09:45:55 +0530

    Lab35

---
+++ src/edu/javacore/pract/Lab35.java
@@ -0,0 +1,14 @@
+package edu.javacore.pract;
+
+public class Lab35 {
+	public static void main(String[] args) {
+		char ch1 = 'R';
+		char ch2 = 'a';
+		char ch3 = 'u';
+		char ch4 = '*';
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		System.out.println(ch4);
+	}
+}
