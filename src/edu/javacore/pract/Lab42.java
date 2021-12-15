e8b90d7319daab3287689ea03d92254385fbe126
User <User@DESKTOP-98P96A5>
2021-12-15 10:40:08 +0530

    Lab42

---
+++ src/edu/javacore/pract/Lab42.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab42 {
+	public static void main(String[] args) {
+	char ch1= '+';
+	char ch2= ''';
+	System.out.println(ch1);
+	System.out.println(ch2);
+}
+}
