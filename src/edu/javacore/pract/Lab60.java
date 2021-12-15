109682b45c0ed6122f1c1111e943610ec6ad129e
User <User@DESKTOP-98P96A5>
2021-12-15 10:46:59 +0530

    Lab60

---
+++ src/edu/javacore/pract/Lab60.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab60 {
+public static void main(String[] args) {
+	char ch1 = -1;
+	char ch2 = 65536;
+	System.out.println(ch1);
+	System.out.println(ch2);
+}
+}
