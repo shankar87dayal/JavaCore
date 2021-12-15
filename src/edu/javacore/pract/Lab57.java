2f224d4d55d8a6f1fbaee37e89ecce9027a41a06
User <User@DESKTOP-98P96A5>
2021-12-15 10:46:04 +0530

    Lab57

---
+++ src/edu/javacore/pract/Lab57.java
@@ -0,0 +1,12 @@
+package edu.javacore.pract;
+
+public class Lab57 {
+public static void main(String[] args) {
+	char ch1 = 65;
+	char ch2 = 48;
+	char ch3 = 97;
+	System.out.println(ch1);
+	System.out.println(ch2);
+	System.out.println(ch3);
+}
+}
