33c9437e279f78c4b92642c4607e0cdca62656c1
User <User@DESKTOP-98P96A5>
2021-12-15 10:50:48 +0530

    Lab71

---
+++ src/edu/javacore/pract/Lab71.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab71 {
+public static void main(String[] args) {
+	char ch1 = '\101';
+	char ch2 = '\103';
+	System.out.println(ch1);
+	System.out.println(ch2);
+} 
+}
