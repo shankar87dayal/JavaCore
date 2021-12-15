b2d9bbe94eccd9ec6342f3d91381fef14edd92a1
User <User@DESKTOP-98P96A5>
2021-12-15 10:53:07 +0530

    Lab79

---
+++ src/edu/javacore/pract/Lab79.java
@@ -0,0 +1,13 @@
+package edu.javacore.pract;
+
+public class Lab79 {
+	public static void main(String[] args) {
+		String str1 = "ASCII OF A is 65";
+		System.out.println(str1);
+		String str2 = "UNICODE OF A IS \u0041";
+		System.out.println(str2);
+		String str3 = "UNICODE OF A IS \\u0041";
+		System.out.println(str3);
+
+	}
+}
