d92c9ffca537afe1b78855c21d7e04280110b5bb
User <User@DESKTOP-98P96A5>
2021-12-15 10:48:22 +0530

    Lab64

---
+++ src/edu/javacore/pract/Lab64.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab64 {
+public static void main(String[] args) {
+	int a = \u0037;
+	int b = '\u0037';
+	System.out.println(a);
+    System.out.println(b);
+}
+}
