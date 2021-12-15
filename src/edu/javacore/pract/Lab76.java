fdc8e67956f9d938b7cd48991f23b604cba4c97f
User <User@DESKTOP-98P96A5>
2021-12-15 10:52:18 +0530

    Lab76

---
+++ src/edu/javacore/pract/Lab76.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab76 {
+public static void main(String[] args) {
+	String str1 = null;
+	System.out.println(str1);
+	int x = str1.length();
+	System.out.println(x);
+}
+}
