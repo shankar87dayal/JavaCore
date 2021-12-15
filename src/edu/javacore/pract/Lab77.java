8861cdaa59a4add56675e74d766199fa3f88771f
User <User@DESKTOP-98P96A5>
2021-12-15 10:52:34 +0530

    Lab77

---
+++ src/edu/javacore/pract/Lab77.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab77 {
+public static void main(String[] args) {
+	String str1 = "";
+	System.out.println(str1);
+	int x = str1.length();
+	System.out.println(x);
+}
+}
