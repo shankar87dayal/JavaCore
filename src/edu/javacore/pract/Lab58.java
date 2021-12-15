ee12e780cbcf52554a73ca1501afd2b2e4873dff
User <User@DESKTOP-98P96A5>
2021-12-15 10:46:20 +0530

    Lab58

---
+++ src/edu/javacore/pract/Lab58.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab58 {
+public static void main(String[] args) {
+	char ch1 = 260;
+	char ch2 = '?';
+	System.out.println(ch1);
+	System.out.println(ch2);
+}
+}
