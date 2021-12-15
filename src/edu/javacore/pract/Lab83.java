900b46417a5bec3f49eb7042fe21ae0462d0bb89
User <User@DESKTOP-98P96A5>
2021-12-15 10:54:16 +0530

    Lab83

---
+++ src/edu/javacore/pract/Lab83.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab83 {
+public static void main(String[] args) {
+	String dir1 = "Octal of 65 is \101";
+	System.out.println(dir1);
+	String dir2 = "Octal of 65 is \\101";
+	System.out.println(dir2);
+}
+}
