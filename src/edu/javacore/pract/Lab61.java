24df85fb33fb3e7980d6146715d80ef4e4bdcf32
User <User@DESKTOP-98P96A5>
2021-12-15 10:47:24 +0530

    Lab61

---
+++ src/edu/javacore/pract/Lab61.java
@@ -0,0 +1,14 @@
+package edu.javacore.pract;
+
+public class Lab61 {
+public static void main(String[] args) {
+	char ch1 = 253;
+	System.out.println("(A+B)");
+	System.out.println(ch1);
+	System.out.println();
+	char ch2 = 246;
+	System.out.println("10");
+	System.out.println(ch2);
+	System.out.println("2 = 5");
+}
+}
