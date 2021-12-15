47f99afb3a3651710ac4308e00988aa3f6afb352
User <User@DESKTOP-98P96A5>
2021-12-15 10:46:36 +0530

    Lab59

---
+++ src/edu/javacore/pract/Lab59.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab59 {
+public static void main(String[] args) {
+	char ch1 = 0;
+	char ch2 = 65535;
+	System.out.println(ch1);
+	System.out.println(ch2);
+}
+}
