b6ce47dd348ff31c999e68687e4d2ffa1c20595a
User <User@DESKTOP-98P96A5>
2021-12-15 10:48:01 +0530

    Lab63

---
+++ src/edu/javacore/pract/Lab63.java
@@ -0,0 +1,14 @@
+package edu.javacore.pract;
+
+public class Lab63 {
+public static void main(String[] args) {
+	int a = '\u0061';
+	int b = '\u0062';
+	System.out.println(a);
+    System.out.println(b);
+    int c = '\u0041';
+	int d = '\u0042';
+	System.out.println(c);
+    System.out.println(d);
+}
+}
