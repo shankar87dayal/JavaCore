bea9c02554797db9912c729f6169a18b0d9ab79d
User <User@DESKTOP-98P96A5>
2021-12-15 10:47:39 +0530

    Lab62

---
+++ src/edu/javacore/pract/Lab62.java
@@ -0,0 +1,14 @@
+package edu.javacore.pract;
+
+public class Lab62 {
+public static void main(String[] args) {
+	char ch1 = '\u0061';
+	char ch2 = '\u0062';
+	System.out.println(ch1);
+    System.out.println(ch2);
+    char ch3 = '\u0041';
+	char ch4 = '\u0042';
+	System.out.println(ch3);
+    System.out.println(ch4);
+}
+}
