07b6387680575e0a61c60a80f163403458d82077
User <User@DESKTOP-98P96A5>
2021-12-15 10:53:28 +0530

    Lab80

---
+++ src/edu/javacore/pract/Lab80.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab80 {
+public static void main(String[] args) {
+	String dir1 = "D:\Entertenment\Bollywood";
+	System.out.println(dir1);
+	String dir2 = "D:\\Entertenment\\Bollywood";
+	System.out.println(dir2);
+}
+}
