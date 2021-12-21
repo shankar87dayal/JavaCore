cf42c99699cb7d9dccc1fc97b80787a9c4d1b179
User <User@DESKTOP-98P96A5>
2021-12-21 15:52:52 +0530

    Lab140 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab140.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab140 {
+	public static void main(String[] args) {
+		int a = 123;
+		byte b = a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
