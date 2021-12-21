5623f9eaa1d4916a21d2b21691b910aee612f688
User <User@DESKTOP-98P96A5>
2021-12-21 15:48:47 +0530

    Lab132 Unary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab132.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab132 {
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		char ch2 = +ch1;
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+}
