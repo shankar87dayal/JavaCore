6d7aabb4165a6e7da92d0464c462bbef31c5dbe7
User <User@DESKTOP-98P96A5>
2021-12-21 15:48:24 +0530

    Lab131 Unary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab131.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab131 {
+	public static void main(String[] args) {
+		int a = 99;
+		int b = --a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
