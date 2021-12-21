a23159bbc82288935152492fdcf048ae740a6c11
User <User@DESKTOP-98P96A5>
2021-12-21 15:47:08 +0530

    Lab128 Unary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab128.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab128 {
+	public static void main(String[] args) {
+		byte a = 19;
+		byte b = +a;
+		byte c = -a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
