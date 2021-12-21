03199b01cbfb11e28d0f6abcb16bc27fced4a65e
User <User@DESKTOP-98P96A5>
2021-12-21 15:51:55 +0530

    Lab138 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab138.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab138 {
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		char ch2 = 'B';
+		char ch3 = ch1 + ch2;
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+	}
+}
