c7d70d369bbc44d8f3c580b08075598991ea4915
User <User@DESKTOP-98P96A5>
2021-12-21 15:55:51 +0530

    Lab146 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab146.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab146 {
+	public static void main(String[] args) {
+		final byte b1 = 12;
+		final long a = 12;
+		byte b2 = b1 + a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
