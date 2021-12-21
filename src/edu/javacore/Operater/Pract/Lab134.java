0b703b357ba93663db26950870c8cec20752c6c1
User <User@DESKTOP-98P96A5>
2021-12-21 15:49:26 +0530

    Lab134 Unary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab134.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab134 {
+	public static void main(String[] args) {
+		double d1 = 1234.567;
+		double d2 = -d1;
+		double d3 = +d1;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+	}
+}
