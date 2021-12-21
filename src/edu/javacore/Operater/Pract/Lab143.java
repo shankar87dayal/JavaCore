5374d481d603f0d1314720381a528ef2706dcf31
User <User@DESKTOP-98P96A5>
2021-12-21 15:53:42 +0530

    Lab143 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab143.java
@@ -0,0 +1,13 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab143 {
+	public static void main(String[] args) {
+		final byte b1 = 90;
+	    byte b2 = 9;
+		byte b3 = b1 + b2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+
+	}
+}
