2dae9bfc744007d075079c1e83f9132f79587aca
User <User@DESKTOP-98P96A5>
2021-12-22 12:20:13 +0530

    Lab189 Type_casting program

---
+++ src/edu/javacore/Type_Casting/Pract/Lab189.java
@@ -0,0 +1,18 @@
+package edu.javacore.Type_Casting.Pract;
+
+public class Lab189 {
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = b;
+		int a = s;
+		long x = a;
+		float f = x;
+		double d = f;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(a);
+		System.out.println(x);
+		System.out.println(f);
+		System.out.println(d);
+	}
+}
