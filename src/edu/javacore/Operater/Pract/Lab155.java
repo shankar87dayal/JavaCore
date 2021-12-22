fbb55eccbd1cf51cfd8c76292e40110cff16c832
User <User@DESKTOP-98P96A5>
2021-12-22 11:59:21 +0530

    Lab155 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab155.java
@@ -0,0 +1,9 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab155 {
+	public static void main(String[] args) {
+		System.out.println(20 / 3);
+		System.out.println(20 / 3.0f);
+		System.out.println(20 / 3.0);
+	}
+}
