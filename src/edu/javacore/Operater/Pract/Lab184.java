806c9b254c334750adac082ac2289916232c09a5
User <User@DESKTOP-98P96A5>
2021-12-22 12:16:15 +0530

    Lab184 Simple Assignment Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab184.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab184 {
+	public static void main(String[] args) {
+		int a = 123;
+		int b = 12 + 23;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
