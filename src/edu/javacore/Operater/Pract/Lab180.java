fd43b389b4d0a6c8e3ff16eff7a223f595d8f381
User <User@DESKTOP-98P96A5>
2021-12-22 12:12:17 +0530

    Lab180 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab180.java
@@ -0,0 +1,15 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab180 {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 10;
+		int c = a + b;
+		System.out.println("sum of");
+		System.out.println(a);
+		System.out.println("and");
+		System.out.println(b);
+		System.out.println("is");
+		System.out.println(c);
+	}
+}
