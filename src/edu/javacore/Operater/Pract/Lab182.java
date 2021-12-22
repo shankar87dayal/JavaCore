04d37bf5fff0311a101034532f0dca2b4c7baee4
User <User@DESKTOP-98P96A5>
2021-12-22 12:14:27 +0530

    Lab182 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab182.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab182 {
+public static void main(String[] args) {
+	int a = 12;
+	int b = 10;
+	int s1 = a  + b;
+	System.out.printf("\n sum of %d and %d is %d. \n", a,b,s1);
+}
+}
