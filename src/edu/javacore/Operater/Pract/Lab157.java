f9b403307162811ce0aa28c9516777ffc73acaf0
User <User@DESKTOP-98P96A5>
2021-12-22 12:00:00 +0530

    Lab157 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab157.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab157 {
+	public static void main(String[] args) {
+		System.out.println(0 / 0.0);
+		System.out.println(0.0 / 0);
+		System.out.println(-0.0 / 0.0);
+		System.out.println(0.0 / 0.0);
+		double d1 = 0 / 0.0;
+		System.out.println(d1);
+	}
+}
