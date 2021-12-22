6a39d8b50dd33694ffaae0e1e071c62f499b665b
User <User@DESKTOP-98P96A5>
2021-12-22 12:21:58 +0530

    Lab193 Type_casting program

---
+++ src/edu/javacore/Type_Casting/Pract/Lab193.java
@@ -0,0 +1,13 @@
+package edu.javacore.Type_Casting.Pract;
+
+public class Lab193 {
+	public static void main(String[] args) {
+		int a = -1;
+		char ch = (char) a;
+		System.out.println(a);
+		System.out.println(ch);
+		int c = ch;
+		System.out.println(c);
+
+	}
+}
