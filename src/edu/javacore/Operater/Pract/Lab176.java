6897557bb696c133b7b0efdb38fa460219fead41
User <User@DESKTOP-98P96A5>
2021-12-22 12:07:54 +0530

    Lab176 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab176.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab176 {
+	public static void main(String[] args) {
+		int a = 90;
+		int b = 9;
+		System.out.println("result is " + a + -b);
+
+	}
+}
