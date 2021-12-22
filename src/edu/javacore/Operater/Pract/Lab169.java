3bad50c1d34c20c81c06218b39f4e1ecb7a6dbcb
User <User@DESKTOP-98P96A5>
2021-12-22 12:04:30 +0530

    Lab169 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab169.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab169 {
+	public static void main(String[] args) {
+		int a = 90;
+		int b = 9;
+		System.out.println("sum of a + b = " + a + b);
+
+	}
+}
