4634992795626b001bfcab148346109f38ca1d3a
User <User@DESKTOP-98P96A5>
2021-12-22 12:22:34 +0530

    Lab195 Type_casting program

---
+++ src/edu/javacore/Type_Casting/Pract/Lab195.java
@@ -0,0 +1,12 @@
+package edu.javacore.Type_Casting.Pract;
+
+public class Lab195 {
+	public static void main(String[] args) {
+		byte b1 = 12;
+		byte b2 = 23;
+		byte b3 = (byte) b1 + b2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+	}
+}
