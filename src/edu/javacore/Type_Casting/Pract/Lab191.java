acbe9cbf1c70bc6465add97180a1da82654a6bc5
User <User@DESKTOP-98P96A5>
2021-12-22 12:21:24 +0530

    Lab191 Type_casting program

---
+++ src/edu/javacore/Type_Casting/Pract/Lab191.java
@@ -0,0 +1,10 @@
+package edu.javacore.Type_Casting.Pract;
+
+public class Lab191 {
+	public static void main(String[] args) {
+		float f = 123.455f;
+		int a = (int) f;
+		System.out.println(f);
+		System.out.println(a);
+	}
+}
