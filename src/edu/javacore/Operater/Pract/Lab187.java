37ec82a3de6e2d22fb382b812bbfb0437aa832a3
User <User@DESKTOP-98P96A5>
2021-12-22 12:17:35 +0530

    Lab187 Simple Assignment Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab187.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab187 {
+	public static void main(String[] args) {
+		int a = 123l;
+		long b = 123.45;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
