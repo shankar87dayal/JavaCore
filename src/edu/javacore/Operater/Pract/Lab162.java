669ae8267ad471ece90eb97016154a95fb917e0e
User <User@DESKTOP-98P96A5>
2021-12-22 12:02:20 +0530

    Lab162 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab162.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab162 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 4;
+		int d = a + b + c;
+		System.out.println(d);
+	}
+}
