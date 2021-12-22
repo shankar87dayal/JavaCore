d3122f571664a1d660937825cb5cf20d35c0c5e6
User <User@DESKTOP-98P96A5>
2021-12-22 12:08:09 +0530

    Lab177 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab177.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab177 {
+	public static void main(String[] args) {
+		int a = 90;
+		String str = a;
+		System.out.println(a);
+		System.out.println(str);
+	}
+}
