6fe50d2d80b4ba0e41f263e42a55d7513655b8fa
User <User@DESKTOP-98P96A5>
2021-12-22 12:18:00 +0530

    Lab188 Simple Assignment Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab188.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab188 {
+	public static void main(String[] args) {
+		long a = 123l;
+		double b = 123.456;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
