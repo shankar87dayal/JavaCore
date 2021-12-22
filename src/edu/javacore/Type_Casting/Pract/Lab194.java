1594c62b2cf1ff1d4856ba288aa57414cf018c00
User <User@DESKTOP-98P96A5>
2021-12-22 12:22:17 +0530

    Lab194 Type_casting program

---
+++ src/edu/javacore/Type_Casting/Pract/Lab194.java
@@ -0,0 +1,12 @@
+package edu.javacore.Type_Casting.Pract;
+
+public class Lab194 {
+	public static void main(String[] args) {
+		byte b1 = 12;
+		byte b2 = 23;
+		byte b3 = b1 + b2;
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+	}
+}
