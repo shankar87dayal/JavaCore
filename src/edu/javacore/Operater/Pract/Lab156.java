f460e37c00df8ce4e9a9088d2678906a83b8c340
User <User@DESKTOP-98P96A5>
2021-12-22 11:59:40 +0530

    Lab156 Binary Arithmetic Operator problem

---
+++ src/edu/javacore/Operater/Pract/Lab156.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater.Pract;
+
+public class Lab156 {
+	public static void main(String[] args) {
+		System.out.println(20 / 0.0);
+		System.out.println(20 / -0.0);
+		System.out.println(20.0 / 0);
+		System.out.println(-20.0 / 0);
+		double d1 = 10 / 0.0;
+		System.out.println(d1);
+	}
+}
