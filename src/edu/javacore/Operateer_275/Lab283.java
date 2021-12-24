9a245bf13b366eadb449a3b41ded3f68f0c57ada
User <User@DESKTOP-98P96A5>
2021-12-24 06:41:50 +0530

    Lab283 Bitwise OR operator program

---
+++ src/edu/javacore/Operateer_275/Lab283.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operateer_275;
+
+public class Lab283 {
+	public static void main(String[] args) {
+		int a = 12;
+		boolean b = a > 10 | a++ < 5;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
