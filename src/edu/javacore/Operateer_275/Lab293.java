293cbdc182215eabc85a2dea99ae84c0e0c94784
User <User@DESKTOP-98P96A5>
2021-12-24 07:02:07 +0530

    Lab293 Right Shift Operator Program

---
+++ src/edu/javacore/Operateer_275/Lab293.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operateer_275;
+
+public class Lab293 {
+	public static void main(String[] args) {
+		int a = 60;
+		System.out.println(a >> 32);
+		System.out.println(a >> 33);
+		System.out.println(a >> 32L);
+		System.out.println(a >> 33L);
+	}
+}
