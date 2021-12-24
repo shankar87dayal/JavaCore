ff907a5291d010b94ebf54fae7e2e780070fc2d7
User <User@DESKTOP-98P96A5>
2021-12-24 06:37:37 +0530

    Lab280 Bitwise AND operator program

---
+++ src/edu/javacore/Operateer_275/Lab280.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operateer_275;
+
+public class Lab280 {
+	public static void main(String[] args) {
+		int a = 12;
+		boolean b = a > 15 & a++ < 5;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
