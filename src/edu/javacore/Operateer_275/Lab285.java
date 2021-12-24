8adbbbfd23f70820a0ecf753595781ccc1b122ed
User <User@DESKTOP-98P96A5>
2021-12-24 06:45:02 +0530

    Lab285 Exclusive OR operator program

---
+++ src/edu/javacore/Operateer_275/Lab285.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operateer_275;
+
+public class Lab285 {
+	public static void main(String[] args) {
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+
+	}
+}
