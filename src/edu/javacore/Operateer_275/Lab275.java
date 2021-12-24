39e2f3b583dbe0a1587081ca41b0955a060039ff
User <User@DESKTOP-98P96A5>
2021-12-24 06:23:18 +0530

    Lab275 Bitwise not operator program

---
+++ src/edu/javacore/Operateer_275/Lab275.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operateer_275;
+
+public class Lab275 {
+	public static void main(String[] args) {
+		int a = 15;
+		int b = ~a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
