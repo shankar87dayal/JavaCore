37eea629f8162cc743dd73e94aff69f683a5f9ac
User <User@DESKTOP-98P96A5>
2021-12-24 06:53:52 +0530

    Lab288 Left Shift Operator Program

---
+++ src/edu/javacore/Operateer_275/Lab288.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operateer_275;
+
+public class Lab288 {
+public static void main(String[] args) {
+	int a = -35425345;
+	System.out.println(a << 0);
+	System.out.println(a << 3);
+	System.out.println(a << 6);
+	System.out.println(a << 13);
+}
+}
