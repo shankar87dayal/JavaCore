cc7df41685942a955af543c87f78ea6f617d8f0f
User <User@DESKTOP-98P96A5>
2021-12-24 07:06:05 +0530

    Lab295 Unsigned Right Shift Operator Program

---
+++ src/edu/javacore/Operateer_275/Lab295.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operateer_275;
+
+public class Lab295 {
+	public static void main(String[] args) {
+		int a = 60;
+		System.out.println(a >>> 0);
+		System.out.println(a >>>1);
+		System.out.println(a >>> 2);
+		System.out.println(a >>> 29);
+	}
+}
