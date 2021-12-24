d031a60c49a6aa7c6cbd602cd4d4cdb71e8480d7
User <User@DESKTOP-98P96A5>
2021-12-24 06:46:31 +0530

    Lab286 Exclusive OR operator program

---
+++ src/edu/javacore/Operateer_275/Lab286.java
@@ -0,0 +1,13 @@
+package edu.javacore.Operateer_275;
+
+public class Lab286 {
+	public static void main(String[] args) {
+		int a = 60; // 00111100
+		int b = 13; // 00001101
+		int c = a ^ b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+}
