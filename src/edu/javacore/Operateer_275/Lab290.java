ce8caab844a96ce69359f2b93fd6e745d615c8e1
User <User@DESKTOP-98P96A5>
2021-12-25 06:55:13 +0530

    Lab290 Left Shift Operator Program

--- src/edu/javacore/Operateer_275/Lab290.java
+++ src/edu/javacore/Operateer_275/Lab290.java
@@ -2,7 +2,7 @@ package edu.javacore.Operateer_275;
 
 public class Lab290 {
 public static void main(String[] args) {
-	int a = 60;
+	long a = 60;
 	System.out.println(a << 32);
 	System.out.println(a << 33);
 	System.out.println(a << 64);
