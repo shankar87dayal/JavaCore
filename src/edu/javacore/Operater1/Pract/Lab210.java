ff1c920b5dc75f6db00adf011650c1fc313fa97d
User <User@DESKTOP-98P96A5>
2021-12-26 18:39:28 +0530

    Lab210 Increment & Decrement Operator Program

---
+++ src/edu/javacore/Operater1/Pract/Lab210.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater1.Pract;
+
+public class Lab210 {
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+	}
+}
