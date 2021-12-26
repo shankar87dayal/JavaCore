9b54769c2952144ecbeba8a00986d8d06bdae3f3
User <User@DESKTOP-98P96A5>
2021-12-26 18:30:34 +0530

    Lab201 Compound Assignment OPerator program

---
+++ src/edu/javacore/Operater1/Pract/Lab201.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater1.Pract;
+
+public class Lab201 {
+	public static void main(String[] args) {
+		int a = 90;
+		System.out.println(a);
+		a += 9;
+		System.out.println(a);
+	}
+}
