432126e52a0645d3e9f7421fde53910543fa8f90
User <User@DESKTOP-98P96A5>
2021-12-27 14:40:43 +0530

    Lab311 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab311.java
@@ -0,0 +1,14 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab311 {
+	public static void main(String[] args) {
+		int a;
+		int x= 99;
+		if (x == 99) {
+			a = 88;
+		}else {
+			a=77;
+		}
+		System.out.println(a);
+	}
+}
