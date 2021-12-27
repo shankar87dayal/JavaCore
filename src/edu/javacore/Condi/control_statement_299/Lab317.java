d18e7148080e321a2653e531928f54b00d0e9bef
User <User@DESKTOP-98P96A5>
2021-12-27 19:29:39 +0530

    Lab317 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab317.java
@@ -0,0 +1,12 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab317 {
+public static void main(String[] args) {
+	int a =10;
+	if(a!=10) {
+	else {
+		System.out.println("Else block");
+	}
+	}
+}
+}
