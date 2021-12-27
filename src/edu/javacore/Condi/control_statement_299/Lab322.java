7628360043cec489aa8326ae59d0eb422f959d5c
User <User@DESKTOP-98P96A5>
2021-12-27 19:44:09 +0530

    Lab322 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab322.java
@@ -0,0 +1,13 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab322 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if (a > b)
+			System.out.println("max value b/w " + a + " and " + b + "is " + a);
+		else
+			System.out.println("max value b/w " + a + " and " + b + "is " + b);
+
+	}
+}
