a6c9ab2985882c375930c4ea105e314e65e8f5b6
User <User@DESKTOP-98P96A5>
2021-12-27 19:18:16 +0530

    Lab313 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab313.java
@@ -0,0 +1,11 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab313 {
+	public static void main(String[] args) {
+		int a = 42;
+		if (a % 2 == 0)
+			System.out.println("value is even");
+		else
+			System.out.println("value is odd");
+	}
+}
