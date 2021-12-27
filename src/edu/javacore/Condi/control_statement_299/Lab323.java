00602043634fcb6c318bd1729d53ea4599efe796
User <User@DESKTOP-98P96A5>
2021-12-27 19:45:29 +0530

    Lab323 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab323.java
@@ -0,0 +1,13 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab323 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if (a < b)
+			System.out.println("min value b/w " + a + " and " + b + "is " + a);
+		else
+			System.out.println("min value b/w " + a + " and " + b + "is " + b);
+
+	}
+}
