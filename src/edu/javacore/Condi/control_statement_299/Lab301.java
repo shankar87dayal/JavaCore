c550a0f68d388706fa1c4c69a0c8450ea5fe3195
User <User@DESKTOP-98P96A5>
2021-12-27 14:02:26 +0530

    Lab301 conditinal control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab301.java
@@ -0,0 +1,10 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab301 {
+	public static void main(String[] args) {
+		if (flase) {
+			System.out.println("IF BLOCK");
+			System.out.println("HELLO GUYS");
+		}
+	}
+}
