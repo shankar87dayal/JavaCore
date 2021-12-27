54ba27912e7f76ab45b8930196b31332f8abe738
User <User@DESKTOP-98P96A5>
2021-12-27 14:12:12 +0530

    Lab306 conditinal control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab306.java
@@ -0,0 +1,10 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab306 {
+	public static void main(String[] args) {
+		int a = 99;
+		if (a == 99) 
+			int x= 12;
+			System.out.println("Hello Guys");
+	}
+}
