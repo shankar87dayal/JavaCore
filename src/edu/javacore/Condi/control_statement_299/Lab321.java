a4671461c0f4c36b46b64a668f2a1c242baf2579
User <User@DESKTOP-98P96A5>
2021-12-27 19:38:06 +0530

    Lab321 conditional control statement program

---
+++ src/edu/javacore/Condi/control_statement_299/Lab321.java
@@ -0,0 +1,9 @@
+package edu.javacore.Condi.control_statement_299;
+
+public class Lab321 {
+	public static void main(String[] args) {
+		if (0)
+			System.out.println("IF BLOCK");
+		System.out.println("Hello Guys");
+	}
+}
