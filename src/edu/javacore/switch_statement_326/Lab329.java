09b9e9d38164f9ce2774314ab901ec40cd1d371d
User <User@DESKTOP-98P96A5>
2021-12-28 12:14:38 +0530

    Lab329 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab329.java
@@ -0,0 +1,13 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab329 {
+	public static void main(String[] args) {
+		char ch = 'A';
+		switch (ch) {
+		case 'a':
+			System.out.println("value is a");
+		case 'A':
+			System.out.println("value is A");
+		}
+	}
+}
