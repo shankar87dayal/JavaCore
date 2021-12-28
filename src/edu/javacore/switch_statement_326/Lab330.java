a84f5d29c745e4057cc74288f773364e28e2b3d4
User <User@DESKTOP-98P96A5>
2021-12-28 12:15:01 +0530

    Lab330 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab330.java
@@ -0,0 +1,13 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab330 {
+	public static void main(String[] args) {
+		long a = 30;
+		switch (a) {
+		case 10:
+			System.out.println("TEN");
+		case 30:
+			System.out.println("THIRTY");
+		}
+	}
+}
