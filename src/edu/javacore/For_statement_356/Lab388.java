54b9041088caa7f36de9f6ff9b40fee834375bfb
User <User@DESKTOP-98P96A5>
2021-12-28 18:29:36 +0530

    Lab388 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab388.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab388 {
+	public static void main(String[] args) {
+		byte b = 127;
+		b++;
+		System.out.println(b);
+		for (byte by = 10; by > 0; by++)
+			System.out.println(by);
+	}
+}
