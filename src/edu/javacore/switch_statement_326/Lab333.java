f35b6085bd9521cf24c224283d34eaaef698f868
User <User@DESKTOP-98P96A5>
2021-12-28 12:28:30 +0530

    Lab333 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab333.java
@@ -0,0 +1,13 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab333 {
+	public static void main(String[] args) {
+		String loc = "MKR";
+		switch (loc) {
+		case "BTM":
+			System.out.println("Branch Office");
+		case "MKR":
+			System.out.println("Head Office");
+		}
+	}
+}
