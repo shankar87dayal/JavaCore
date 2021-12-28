0f279d98570166278a9a7b4d423968eb73571b75
User <User@DESKTOP-98P96A5>
2021-12-28 12:41:40 +0530

    Lab335 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab335.java
@@ -0,0 +1,14 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab335 {
+	public static void main(String[] args) {
+		int a = 98;
+		int x = 96;
+		switch (a) {
+		case 98:
+			System.out.println("val - 98");
+		case x:
+			System.out.println("variable - x");
+		}
+	}
+}
