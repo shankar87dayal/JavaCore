27940a19db7040a04f0929a494ca540e059e0605
User <User@DESKTOP-98P96A5>
2021-12-28 19:24:36 +0530

    Lab394 While statement program

---
+++ src/edu/javacore/While_statement_394/Lab394.java
@@ -0,0 +1,11 @@
+package edu.javacore.While_statement_394;
+
+public class Lab394 {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
