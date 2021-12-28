9f9f6e2695dcafb69cfb8d1cde5a4e2096aed326
User <User@DESKTOP-98P96A5>
2021-12-28 17:18:09 +0530

    Lab365 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab365.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab365 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			int a = 99;
+			System.out.println(a);
+		}
+		System.out.println(a);
+	}
+}
