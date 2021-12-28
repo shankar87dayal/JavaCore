56cc1849a94ee8d4c63c54d2f8574c126302471c
User <User@DESKTOP-98P96A5>
2021-12-28 18:24:20 +0530

    Lab385 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab385.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab385 {
+	public static void main(String[] args) {
+		final boolean b1 = true;
+		for (; b1;)
+			System.out.println("Raushan");
+		System.out.println("After");
+	}
+
+}
