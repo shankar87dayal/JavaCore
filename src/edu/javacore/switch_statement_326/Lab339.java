bf425cbbd0409ff0a387523622112dd711c08c04
User <User@DESKTOP-98P96A5>
2021-12-28 12:52:33 +0530

    Lab339 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab339.java
@@ -0,0 +1,18 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab339 {
+	public static void main(String[] args) {
+		int a = 45;
+		final int x = 20;
+		switch (a) {
+		case 10:
+			System.out.println("ten");
+		case x:
+			System.out.println("twenty");
+		case 'A':
+			System.out.println("char -A");
+		case 10 + 20 + 15:
+			System.out.println("CONSTANT EXPR");
+		}
+	}
+}
