ce9420cfc4b15b5d5a2a665d1b5ae2330d01ba77
User <User@DESKTOP-98P96A5>
2021-12-28 17:28:38 +0530

    Lab368 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab368.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab368 {
+	public static void main(String[] args) {
+		int i = 1;
+		for (; i <= 10;) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
