fcca0769bd62ddad7ca6a6d62ecda38b22413f81
User <User@DESKTOP-98P96A5>
2021-12-28 12:06:42 +0530

    Lab328 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab328.java
@@ -0,0 +1,13 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab328 {
+	public static void main(String[] args) {
+		int a = 30;
+		switch (a) {
+		case 10:
+			System.out.println("TEN");
+		case 30:
+			System.out.println("THIRTY");
+		}
+	}
+}
