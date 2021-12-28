253ac476653d18f677cbd10ae4d97e3d4762553a
User <User@DESKTOP-98P96A5>
2021-12-28 17:05:29 +0530

    Lab362 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab362.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab362 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			int a = 99;
+			a++;
+			System.out.println(i + "\t" + a);
+		}
+	}
+}
