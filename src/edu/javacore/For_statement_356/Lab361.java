23a2e3b735e94ec9b2adb682e7ca71a9392766c9
User <User@DESKTOP-98P96A5>
2021-12-28 17:01:31 +0530

    Lab361 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab361.java
@@ -0,0 +1,10 @@
+package edu.javacore.For_statement_356;
+
+public class Lab361 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + "*" + i + "=" + n * i);
+		}
+	}
+}
