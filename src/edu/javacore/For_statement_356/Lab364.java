94b9c35c38094dabc8df3e14c23528c16ec47677
User <User@DESKTOP-98P96A5>
2021-12-28 17:17:37 +0530

    Lab364 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab364.java
@@ -0,0 +1,10 @@
+package edu.javacore.For_statement_356;
+
+public class Lab364 {
+	public static void main(String[] args) {
+		int i = 1;
+		for (; i <= 10; i++)
+			System.out.println(i);
+		System.out.println("After: " + i);
+	}
+}
