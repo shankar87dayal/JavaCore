9646347d18f7730e8364c30157c5b1b48bb06f19
User <User@DESKTOP-98P96A5>
2021-12-28 18:16:29 +0530

    Lab383 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab383.java
@@ -0,0 +1,9 @@
+package edu.javacore.For_statement_356;
+
+public class Lab383 {
+	public static void main(String[] args) {
+		for (;;)
+			System.out.println("Raushan");
+		System.out.println("After");
+	}
+}
