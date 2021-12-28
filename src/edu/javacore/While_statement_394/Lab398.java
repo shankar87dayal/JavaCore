11d9255792b917840bb2880cfc06da3e726ae6e4
User <User@DESKTOP-98P96A5>
2021-12-28 19:34:50 +0530

    Lab398 While Statement program

---
+++ src/edu/javacore/While_statement_394/Lab398.java
@@ -0,0 +1,11 @@
+package edu.javacore.While_statement_394;
+
+public class Lab398 {
+	public static void main(String[] args) {
+		boolean b1 = true;
+		while (b1) {
+			System.out.println("Raushan");
+		}
+		System.out.println("After");
+	}
+}
