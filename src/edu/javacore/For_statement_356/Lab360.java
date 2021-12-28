0da833c1972d307db5944daaa6baecba12704bcf
User <User@DESKTOP-98P96A5>
2021-12-28 16:56:30 +0530

    Lab360 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab360.java
@@ -0,0 +1,12 @@
+package edu.javacore.For_statement_356;
+
+public class Lab360 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 7 == 0)
+				System.out.println(i);
+		}
+	}
+
+
+}
