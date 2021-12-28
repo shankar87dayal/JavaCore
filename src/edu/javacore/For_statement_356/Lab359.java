92427b3a2316cd6eac1451c785022e9b1853038d
User <User@DESKTOP-98P96A5>
2021-12-28 16:54:15 +0530

    Lab359 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab359.java
@@ -0,0 +1,11 @@
+package edu.javacore.For_statement_356;
+
+public class Lab359 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 != 0)
+				System.out.println(i);
+		}
+	}
+
+}
