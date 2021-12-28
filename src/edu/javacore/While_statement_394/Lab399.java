f87c81915d6fe0ba4a85fbadde0b940b5c619115
User <User@DESKTOP-98P96A5>
2021-12-28 19:37:14 +0530

    Lab399 While Statement program

---
+++ src/edu/javacore/While_statement_394/Lab399.java
@@ -0,0 +1,10 @@
+package edu.javacore.While_statement_394;
+
+public class Lab399 {
+	public static void main(String[] args) {
+		int n = 5, i = 1;
+		while (i <= 10) {
+			System.out.println(n * i++);
+		}
+	}
+}
