94915a964851f6db9863332c75a96b55aeea6ed8
User <User@DESKTOP-98P96A5>
2021-12-28 14:26:23 +0530

    Lab349 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab349.java
@@ -0,0 +1,15 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab349 {
+	public static void main(String[] args) {
+		int a = 20;
+		switch (a) {
+		default:
+			System.out.println("DEFAULT");
+		case 10:
+			System.out.println("ten"); 
+				case 10 + 10:
+			System.out.println("twenty");
+		}
+	}
+}
