7825194b61e4fc51830a8ec7103bd6bb1d169494
User <User@DESKTOP-98P96A5>
2021-12-28 12:01:56 +0530

    Lab327 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab327.java
@@ -0,0 +1,31 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab327 {
+	public static void main(String[] args) {
+		int a = 3;
+		switch (a) {
+		case 0:
+			System.out.println("sunday");
+			break;
+		case 1:
+			System.out.println("monday");
+			break;
+		case 2:
+			System.out.println("tuesday");
+			break;
+		case 3:
+			System.out.println("wednesday");
+			break;
+		case 4:
+			System.out.println("thursday");
+			break;
+		case 5:
+			System.out.println("friday");
+			break;
+		case 6:
+			System.out.println("saturday");
+		default:
+			System.out.println("wrong input");
+		}
+	}
+}
