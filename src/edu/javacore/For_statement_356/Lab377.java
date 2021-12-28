807f452e537f4212f1244a8f7fb0be5fed5b7d08
User <User@DESKTOP-98P96A5>
2021-12-28 18:04:28 +0530

    Lab377 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab377.java
@@ -0,0 +1,10 @@
+package edu.javacore.For_statement_356;
+
+public class Lab377 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) 
+		System.out.println("ok");
+	}
+
+}
