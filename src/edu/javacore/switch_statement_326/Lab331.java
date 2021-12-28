1f388baf0c756ad4bb7d952e7c8be867bd3c1eb9
User <User@DESKTOP-98P96A5>
2021-12-28 12:17:19 +0530

    Lab331 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab331.java
@@ -0,0 +1,13 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab331 {
+	public static void main(String[] args) {
+		float  f = 30.0f;
+		switch (f) {
+		case 10.0f:
+			System.out.println("TEN");
+		case 30.0f:
+			System.out.println("THIRTY");
+		}
+	}
+}
