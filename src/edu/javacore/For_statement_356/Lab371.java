dc9416f6fedddaf92837bd5bbcb94554a10a7dbb
User <User@DESKTOP-98P96A5>
2021-12-28 17:35:14 +0530

    Lab371 For statement program

---
+++ src/edu/javacore/For_statement_356/Lab371.java
@@ -0,0 +1,10 @@
+package edu.javacore.For_statement_356;
+
+public class Lab371 {
+	public static void main(String[] args) {
+		for (int i = 1, a = 99; i <= 10; i++) {
+			System.out.println(i + "\t" + a);
+			a++;
+		}
+	}
+}
