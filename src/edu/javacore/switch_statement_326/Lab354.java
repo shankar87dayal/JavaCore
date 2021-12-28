d0eabb3d30e666df9aa185b330b13ead5b288291
User <User@DESKTOP-98P96A5>
2021-12-28 14:45:55 +0530

    Lab354 switch statement program

---
+++ src/edu/javacore/switch_statement_326/Lab354.java
@@ -0,0 +1,19 @@
+package edu.javacore.switch_statement_326;
+
+public class Lab354 {
+	public static void main(String[] args) {
+		int x = 0;
+		switch (x) {
+		case 0: {
+			int a = 10;
+			System.out.println("A1: " + a);
+			break;
+		}
+		case 1: {
+			int a = 10;
+			System.out.println("A2: " + a);
+			break;
+		}
+		}
+	}
+}
