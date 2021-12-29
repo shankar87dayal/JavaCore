cfde8111d9f5aec74deef9950ac2713df4d53fb5
User <User@DESKTOP-98P96A5>
2021-12-29 09:39:34 +0530

    Lab423 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab423.java
@@ -0,0 +1,11 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab423 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++)
+			System.out.println("\n" + i);
+		for (int j = 1; j <= 5; j++, System.out.print("j++")) {
+			System.out.print("Raushan" + j);
+		}
+	}
+}
