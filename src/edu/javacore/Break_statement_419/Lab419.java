c7b87c5ee8538bf9186aa0f47d3be043339504e7
User <User@DESKTOP-98P96A5>
2021-12-29 08:32:51 +0530

    Lab419 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab419.java
@@ -0,0 +1,11 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab419 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			break;
+		}
+		System.out.println("AFTER");
+	}
+}
