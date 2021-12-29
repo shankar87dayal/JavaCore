7847f6fa4adccd53c1e3479aaed9b858ab6285bc
User <User@DESKTOP-98P96A5>
2021-12-29 10:00:52 +0530

    Lab429 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab429.java
@@ -0,0 +1,16 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab429 {
+	public static void main(String[] args) {
+		sd: for (int i = 1; i <= 5; i++, System.out.println("i++")) {
+			System.out.println("\n" + 1);
+			Raushan: for (int j = 1; j <= 5; j++, System.out.println("j++")) {
+				if (i == 3)
+					break Raushan;
+				System.out.println("Raushan" + j);
+			}
+			System.out.println("\nINSIDE I- LOOP,OUTSIDE J-LOOP");
+		}
+		System.out.println("\nOUTSIDE I-LOOP");
+	}
+}
