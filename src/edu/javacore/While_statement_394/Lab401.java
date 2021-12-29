a921713830330afd1853e38bf4943cc8cbb7c882
User <User@DESKTOP-98P96A5>
2021-12-29 07:25:16 +0530

    Lab401 While Statetment Program

---
+++ src/edu/javacore/While_statement_394/Lab401.java
@@ -0,0 +1,11 @@
+package edu.javacore.While_statement_394;
+
+public class Lab401 {
+	public static void main(String[] args) {
+		int n = 5, i = 1;
+		while (i <= 10) {
+			System.out.println(n * i);
+			i++;
+		}
+	}
+}
