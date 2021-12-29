13e997ca2a844e4e032639f6dfb003668b06ae59
User <User@DESKTOP-98P96A5>
2021-12-29 09:47:50 +0530

    Lab426 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab426.java
@@ -0,0 +1,15 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab426 {
+	public static void main(String[] args) {
+		final boolean b1 = true;
+		for (; b1;) {
+			System.out.println("Raushan");
+			int a = 10;
+			if (a == 20) {
+				break;
+			}
+		}
+		System.out.println("AFTER");
+	}
+}
