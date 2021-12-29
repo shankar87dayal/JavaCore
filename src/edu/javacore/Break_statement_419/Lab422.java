1cdcc891eb3249fa843f49ba51091e1c80585ee2
User <User@DESKTOP-98P96A5>
2021-12-29 08:40:29 +0530

    Lab422 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab422.java
@@ -0,0 +1,11 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab422 {
+public static void main(String[] args) {
+	for(int i=0; i<10;i++) {
+		System.out.println(i);
+		break;
+		System.out.println("**INSIDE LOOP");
+	}
+}
+}
