144f06917814b556eac32089c257e9bc309435d0
User <User@DESKTOP-98P96A5>
2021-12-29 11:29:10 +0530

    Lab433 Continue Statement Program

---
+++ src/edu/javacore/continue_statement_433/Lab433.java
@@ -0,0 +1,10 @@
+package edu.javacore.continue_statement_433;
+
+public class Lab433 {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++, System.out.println("Updating")) {
+			System.out.println(i);
+			continue;
+		}
+	}
+}
