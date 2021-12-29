c4c23cfb4a38c8474f0603ceaefbaae79a374f77
User <User@DESKTOP-98P96A5>
2021-12-29 09:45:08 +0530

    Lab425 Break Statement Program

---
+++ src/edu/javacore/Break_statement_419/Lab425.java
@@ -0,0 +1,12 @@
+package edu.javacore.Break_statement_419;
+
+public class Lab425 {
+	public static void main(String[] args) {
+		final boolean b1 = true;
+		for (; b1;) {
+			System.out.println("Ranjan");
+			break;
+		}
+		System.out.println("AFTER");
+	}
+}
