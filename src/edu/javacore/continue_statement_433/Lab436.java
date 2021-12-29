9235b66f20b9a40fd5b6d86dded4dbe60a81d07a
User <User@DESKTOP-98P96A5>
2021-12-29 11:37:26 +0530

    Lab436	 Continue Statement Program

---
+++ src/edu/javacore/continue_statement_433/Lab436.java
@@ -0,0 +1,11 @@
+package edu.javacore.continue_statement_433;
+
+public class Lab436 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+				continue;
+			System.out.println("**AFTER");
+		}
+	}
+}
