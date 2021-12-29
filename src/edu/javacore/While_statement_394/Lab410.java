4e5b16ac0c66d854a5573a782ce80c2b5ae6bcec
User <User@DESKTOP-98P96A5>
2021-12-29 07:59:59 +0530

    Lab410 While Statetment Program

---
+++ src/edu/javacore/While_statement_394/Lab410.java
@@ -0,0 +1,11 @@
+package edu.javacore.While_statement_394;
+
+public class Lab410 {
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 10);
+	}
+}
