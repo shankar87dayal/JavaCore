c557856d716f7673bf0bbf2d42d0c300508fe05b
User <User@DESKTOP-98P96A5>
2021-12-29 12:00:05 +0530

    Lab445 Continue Statement Program

---
+++ src/edu/javacore/continue_statement_433/Lab445.java
@@ -0,0 +1,8 @@
+package edu.javacore.continue_statement_433;
+
+public class Lab445 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to chennai");
+		System.out.println("message storing in file");
+	}
+}
