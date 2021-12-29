3957b45aeb3eef02be876c006f842d88f9bb20e9
User <User@DESKTOP-98P96A5>
2021-12-29 07:55:15 +0530

    Lab408 While Statetment Program

---
+++ src/edu/javacore/While_statement_394/Lab408.java
@@ -0,0 +1,13 @@
+package edu.javacore.While_statement_394;
+
+public class Lab408 {
+	public static void main(String[] args) {
+		int a = 0;
+		while (true) {
+			System.out.println("value of a: " + a);
+			a--;
+			if (a <= 20)
+				break;
+		}
+	}
+}
