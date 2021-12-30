1e239b9716ff989983b3f908f1aeb62452f90d59
User <User@DESKTOP-98P96A5>
2021-12-30 11:12:45 +0530

    Lab452 Array Initialization problem

---
+++ src/edu/javacore/Array/Dec_446/Lab452.java
@@ -0,0 +1,18 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab452 {
+
+	public static void main(String[] args) {
+		int arr[];
+		arr = new int[3];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+		System.out.println("----");
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
+
+}
