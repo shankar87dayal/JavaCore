09b1c090f22b175d8f06f79b3df522a809471c0d
User <User@DESKTOP-98P96A5>
2021-12-30 11:37:28 +0530

    Lab457 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab457.java
@@ -0,0 +1,13 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab457 {
+	public static void main(String[] args) {
+		String arr[]= new String[3];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+		System.out.println("----");
+		arr[0]="Raushan"; arr[1]="Ranjan"; arr[2]="ji";
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
+}
