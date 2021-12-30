6b73d382112068b513ec64a5e630f02b2a0b43ec
User <User@DESKTOP-98P96A5>
2021-12-30 11:15:57 +0530

    Lab453 Array Initialization problem

---
+++ src/edu/javacore/Array/Dec_446/Lab453.java
@@ -0,0 +1,16 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab453 {
+	public static void main(String[] args) {
+		double arr[];
+		arr = new double[3];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+		System.out.println("----");
+		arr[0]=10.99;
+		arr[1]=20.56;
+		arr[2]=30.23;
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
+}
