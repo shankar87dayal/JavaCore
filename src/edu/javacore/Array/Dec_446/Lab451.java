055fc3ee0841d1fbdd032e373987e6ae46aae7a0
User <User@DESKTOP-98P96A5>
2021-12-30 11:07:06 +0530

    Lab451 Array Initialization problem

---
+++ src/edu/javacore/Array/Dec_446/Lab451.java
@@ -0,0 +1,10 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab451 {
+	public static void main(String[] args) {
+		int arr[];
+		arr = new int[3];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[0]);
+	}
+}
