ee7897fb626defe5db8b4dbbe92c8c01de64d951
User <User@DESKTOP-98P96A5>
2021-12-30 10:58:34 +0530

    Lab448 Array Declaration problem

---
+++ src/edu/javacore/Array/Dec_446/Lab448.java
@@ -0,0 +1,15 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab448 {
+	public static void main(String[] args) {
+		int arr1[];
+		float arr2[];
+		String names[];
+		arr1 = new int[3];
+		arr2 = new float[5];
+		names = new String[4];
+		System.out.println(arr1.length);
+		System.out.println(arr2.length);
+		System.out.println(names.length);
+	}
+}
