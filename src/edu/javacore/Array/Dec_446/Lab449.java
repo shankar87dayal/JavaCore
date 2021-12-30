8577cd6d29e07b47af0c39136077379e101e867b
User <User@DESKTOP-98P96A5>
2021-12-30 11:01:12 +0530

    Lab449 Array Declaration problem

---
+++ src/edu/javacore/Array/Dec_446/Lab449.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab449 {
+	public static void main(String[] args) {
+		int arr1[];
+		arr1 = new int[];
+		System.out.println(arr1.length);
+	}
+}
