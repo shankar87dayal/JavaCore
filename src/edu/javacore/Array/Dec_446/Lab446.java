70dd2083e7226ea4f462d5f3aae143c5e5d3cff5
User <User@DESKTOP-98P96A5>
2021-12-30 10:49:01 +0530

    Lab446 Array Declaration problem

---
+++ src/edu/javacore/Array/Dec_446/Lab446.java
@@ -0,0 +1,11 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab446 {
+	static int arr[];
+	static String names[];
+
+	public static void main(String[] args) {
+		System.out.println(arr);
+		System.out.println(names);
+	}
+}
