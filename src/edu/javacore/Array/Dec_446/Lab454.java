71022caf24c2a9e9d50da0153c8e41763da2d72a
User <User@DESKTOP-98P96A5>
2021-12-30 11:18:23 +0530

    Lab454 Array Initialization problem

---
+++ src/edu/javacore/Array/Dec_446/Lab454.java
@@ -0,0 +1,16 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab454 {
+	public static void main(String[] args) {
+		String arr[];
+		arr = new String[3];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+		System.out.println("----");
+		arr[0]="Raushan";
+		arr[1]="Ranjan";
+		arr[2]="ji";
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
+}
