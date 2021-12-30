0dd956ea5ea1254501a6baea5f331c852f0f088b
User <User@DESKTOP-98P96A5>
2021-12-30 11:04:41 +0530

    Lab450 Array Initialization problem

---
+++ src/edu/javacore/Array/Dec_446/Lab450.java
@@ -0,0 +1,11 @@
+package edu.javacore.Array.Dec_446;
+
+public class Lab450 {
+	public static void main(String[] args) {
+		int arr[];
+		arr = new int[3];
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+	}
+}
