ba3c32e0784202db9cf35d4a371391db3fa29777
User <User@DESKTOP-98P96A5>
2021-12-31 10:58:58 +0530

    Lab472 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab472.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab472 {
+	public static void main(String[] args) {
+		int arr[] = new int[0];
+		System.out.println("LEN: " + arr.length);
+		System.out.println(arr[0]);
+	}
+}
