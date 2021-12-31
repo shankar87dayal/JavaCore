b22540708940ceac468c3320c8a20bfb8b719a6f
User <User@DESKTOP-98P96A5>
2021-12-31 15:02:45 +0530

    Lab492 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab492.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab492 {
+	public static void main(String[] args) {
+		Object obj[] = new Object[3];
+		for (int i = 0; i < obj.length; i++)
+			System.out.println(obj[i]);
+	}
+}
