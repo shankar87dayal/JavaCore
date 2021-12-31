4782a592d0010b9617ce1d0be552ca26c3060b94
User <User@DESKTOP-98P96A5>
2021-12-31 10:14:50 +0530

    Lab463 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab463.java
@@ -0,0 +1,8 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab463 {
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		System.out.println(arr.length);
+	}
+}
