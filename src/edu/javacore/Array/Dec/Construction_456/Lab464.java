1b5c02e211c53eb24a74a8812bc823dd2e48f5b3
User <User@DESKTOP-98P96A5>
2021-12-31 10:17:15 +0530

    Lab464 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab464.java
@@ -0,0 +1,8 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab464 {
+	public static void main(String[] args) {
+		int arr[] = new int[10 + 20 / 5];
+		System.out.println(arr.length);
+	}
+}
