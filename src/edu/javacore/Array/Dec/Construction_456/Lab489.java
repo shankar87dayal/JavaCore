a68679902f67bffeb2dfb240c6d79142e0d66859
User <User@DESKTOP-98P96A5>
2021-12-31 14:52:38 +0530

    Lab489 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab489.java
@@ -0,0 +1,16 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab489 {
+public static void main(String[] args) {
+	final int arr[]=new int [2];
+	System.out.println("len: "+arr.length);
+	System.out.println(arr[0]);
+	System.out.println(arr[1]);
+	System.out.println();
+	arr[0]=99;
+	arr[1]=88;
+	System.out.println(arr[0]);
+	System.out.println(arr[1]);
+	
+}
+}
