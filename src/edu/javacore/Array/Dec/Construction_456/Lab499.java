57182c0c1cac00c687b7f97d516d1f13e40e21ce
User <User@DESKTOP-98P96A5>
2021-12-31 15:21:43 +0530

    Lab499 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab499.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab499 {
+	public static void main(String[] args) {
+		byte brr[] = new byte[3];
+		int arr[] = brr;
+		System.out.println("Hello guys");
+	}
+}
