c5978c408c4fdbf90d7736f271933b02cdf4ad98
User <User@DESKTOP-98P96A5>
2021-12-31 20:31:16 +0530

    Lab511 Anonymous Array problem

---
+++ src/edu/javacore/Anonymous/Array_511/Lab511.java
@@ -0,0 +1,11 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab511 {
+public static void main(String[] args) {
+	int arr[];
+	arr = {99,88,66,77};
+	System.out.println("Len:"+arr.length);
+	for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+}
+}
