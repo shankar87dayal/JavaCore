08f6ea3fbece75a5e1396e95710a4ae126ce6456
User <User@DESKTOP-98P96A5>
2021-12-31 11:06:46 +0530

    Lab476 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab476.java
@@ -0,0 +1,8 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab476 {
+	public static void main(String[] args) {
+		int arr[]=null;
+		System.out.println("LEN: " + arr.length);
+	}
+}
