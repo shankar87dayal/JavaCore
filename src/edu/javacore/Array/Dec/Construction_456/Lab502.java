32aaba5a9792daa5730572eda7751084433bb79d
User <User@DESKTOP-98P96A5>
2021-12-31 15:32:29 +0530

    Lab502 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab502.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab502 {
+public static void main(String[] args) {
+	int arr[]=new int[3];
+	arr[0]=10; arr[1]=20; arr[2]=30;
+	System.out.println(arr);
+}
+}
