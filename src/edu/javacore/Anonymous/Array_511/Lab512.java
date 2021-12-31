4ade8a2a50eb7edff9a37dffb760a78dddeda6f6
User <User@DESKTOP-98P96A5>
2021-12-31 20:36:19 +0530

    Lab512 Anonymous Array problem

---
+++ src/edu/javacore/Anonymous/Array_511/Lab512.java
@@ -0,0 +1,14 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab512 {
+public static void main(String[] args) {
+	int arr[]=null;
+	arr = new int[] {99,88,66,77};
+	show(arr);
+	
+}
+static void show(int arr[]) {
+	for(int i=0;i<arr.length;i++)
+	System.out.println(arr[i]);
+}
+}
