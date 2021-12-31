eb2016ef77aa592997056c1570dd7d9c8619e5c0
User <User@DESKTOP-98P96A5>
2021-12-31 20:16:22 +0530

    Lab507 Array Declaration, construction & initialization problem

---
+++ src/edu/javacore/Array/Dec/cons/and/initialization_505/Lab507.java
@@ -0,0 +1,10 @@
+package edu.javacore.Array.Dec.cons.and.initialization_505;
+
+public class Lab507 {
+	public static void main(String[] args) {
+		int arr[] = {};
+		System.out.println("Len: " + arr.length);
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+	}
+}
