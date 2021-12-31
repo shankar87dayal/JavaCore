56231c2b368d7857af4849d85c622ae8f06fd3be
User <User@DESKTOP-98P96A5>
2021-12-31 13:46:53 +0530

    Lab481 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab481.java
@@ -0,0 +1,11 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab481 {
+	public static void main(String[] args) {
+		int arr1[], arr2;
+		arr1 = new int[2];
+		arr2 = 123;
+		System.out.println(arr1.length);
+		System.out.println(arr2);
+	}
+}
