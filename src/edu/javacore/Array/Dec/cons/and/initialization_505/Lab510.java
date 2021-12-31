4414004cc2ebd1ac9bdc3f7cdfef158fb90a6eb1
User <User@DESKTOP-98P96A5>
2021-12-31 20:23:33 +0530

    Lab510 Array Declaration, construction & initialization problem

---
+++ src/edu/javacore/Array/Dec/cons/and/initialization_505/Lab510.java
@@ -0,0 +1,10 @@
+package edu.javacore.Array.Dec.cons.and.initialization_505;
+
+public class Lab510 {
+	public static void main(String[] args) {
+	String names[] = {"Mr","Raushan","Ranjan"};
+		System.out.println("Len: " + names.length);
+		for (int i = 0; i < names.length; i++)
+			System.out.println(names[i]);
+	}
+}
