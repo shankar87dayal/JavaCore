019f9c1c4f18990aa46be0f7ab8193a7cd213ca6
User <User@DESKTOP-98P96A5>
2021-12-31 15:29:35 +0530

    Lab501 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab501.java
@@ -0,0 +1,9 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab501 {
+public static void main(String[] args) {
+	char ch[]=new char[7];
+	ch[0]='R'; ch[1]='a'; ch[2]='u'; ch[3]='s'; ch[4]='h'; ch[5]='a'; ch[6]='n';
+	System.out.println(ch);
+}
+}
