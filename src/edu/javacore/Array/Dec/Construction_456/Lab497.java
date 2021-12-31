41e45085ad25a61b529bc93031e0933f779da934
User <User@DESKTOP-98P96A5>
2021-12-31 15:12:58 +0530

    Lab497 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab497.java
@@ -0,0 +1,10 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab497 {
+	public static void main(String[] args) {
+		byte b = 99;
+		int a = b;
+		System.out.println(b);
+		System.out.println(a);
+	}
+}
