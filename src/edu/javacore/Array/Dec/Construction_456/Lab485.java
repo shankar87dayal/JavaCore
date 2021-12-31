51df8efd5525ad550e7a783440ddbc5aa8bbdcd5
User <User@DESKTOP-98P96A5>
2021-12-31 13:56:57 +0530

    Lab485 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab485.java
@@ -0,0 +1,10 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab485 {
+	public static void main(String[] args) {
+		int arr[] = new int[3];
+		System.out.println(arr.length);
+		arr.length = 30;
+		System.out.println(arr.length);
+	}
+}
