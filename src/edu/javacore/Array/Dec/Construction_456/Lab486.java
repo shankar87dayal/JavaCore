6084d759fd0f4898dc39f8a7f6d4d3ed437dff8a
User <User@DESKTOP-98P96A5>
2021-12-31 14:13:55 +0530

    Lab486 Array Declaration & construction problem

---
+++ src/edu/javacore/Array/Dec/Construction_456/Lab486.java
@@ -0,0 +1,15 @@
+package edu.javacore.Array.Dec.Construction_456;
+
+public class Lab486 {
+	public static void main(String[] args) {
+		int arr[] = new int[3];
+		System.out.println("len:" + arr.length);
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+		arr = new int[5];
+		System.out.println("lan: " + arr.length);
+		for (int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);
+
+	}
+}
