3f89334d1613e29b2ca7b0796b68f8e948d5d8e9
User <User@DESKTOP-98P96A5>
2022-01-01 12:03:19 +0530

    Lab522 Enhanced  For Statement problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab522.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab522 {
+	public static void main(String[] args) {
+		char arr[] = { 'c', 'h', 'e', 'n', 'n', 'a', 'i' };
+		for (char ch : arr) {
+			System.out.println(ch);
+		}
+	}
+}
