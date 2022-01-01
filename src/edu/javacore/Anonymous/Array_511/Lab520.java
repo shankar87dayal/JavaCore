c02d5ab6c330bc84e38309bf7a4ab9580e86d37e
User <User@DESKTOP-98P96A5>
2022-01-01 11:58:19 +0530

    Lab520 Enhanced  For Statement problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab520.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab520 {
+	public static void main(String[] args) {
+		String names[] = new String[] { "Mr", "Raushan", "Ranjan" };
+		for (String str : names) {
+			System.out.println(str);
+		}
+	}
+}
