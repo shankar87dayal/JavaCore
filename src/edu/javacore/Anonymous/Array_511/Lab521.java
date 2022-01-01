54fc5e2329af99aa0fd28c5142663b0e34766aa6
User <User@DESKTOP-98P96A5>
2022-01-01 11:59:45 +0530

    Lab521 Enhanced  For Statement problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab521.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab521 {
+	public static void main(String[] args) {
+		int arr[] = null;
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
