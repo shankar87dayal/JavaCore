cfb7d546b077b67f2a2cad53d4e91aeebbbdaefe
User <User@DESKTOP-98P96A5>
2022-01-01 11:57:59 +0530

    Lab519 Enhanced  For Statement problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab519.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab519 {
+	public static void main(String[] args) {
+		int arr[] = { 99, 88, 66, 77 };
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
