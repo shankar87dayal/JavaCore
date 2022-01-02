24a3e8c0759c5e6aea042fe8b0a4b614cbb048ae
User <User@DESKTOP-98P96A5>
2022-01-02 10:55:38 +0530

    Lab541

---
+++ src/edu/javacore/Anonymous/Array_511/Lab541.java
@@ -0,0 +1,13 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab541 {
+	public static void main(String[] args) {
+		int a, b, c;
+		a = 12;
+		b = 23;
+		c = 34;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
