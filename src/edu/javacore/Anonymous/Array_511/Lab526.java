2be9193e30fd65446b19315da0dc7ef2d67edaab
User <User@DESKTOP-98P96A5>
2022-01-02 09:43:16 +0530

    Lab526

---
+++ src/edu/javacore/Anonymous/Array_511/Lab526.java
@@ -0,0 +1,9 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab526 {
+	public static void main(String[] args) {
+		int arr[][] = null;
+		arr = new int[3][2];
+		System.out.println("Len: " + arr.length);
+	}
+}
