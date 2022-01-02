69d83800a1948f8d486e92a53a540eab0ac490c5
User <User@DESKTOP-98P96A5>
2022-01-02 10:52:35 +0530

    Lab539

--- src/edu/javacore/Anonymous/Array_511/Lab539.java
+++ src/edu/javacore/Anonymous/Array_511/Lab539.java
@@ -6,11 +6,10 @@ public class Lab539 {
 		arr = new int[][] { { 10, 23, 21 }, { 12 }, { 23, 43, 12, 32 }, { 98, 34, 32 } };
 		for (int ar[] : arr) {
 			for (int a : ar) {
-				System.out.println("\t" + a);
-				System.out.println();
+				System.out.print("\t" + a);
 
 			}
-
+			System.out.println();
 		}
 	}
 }
