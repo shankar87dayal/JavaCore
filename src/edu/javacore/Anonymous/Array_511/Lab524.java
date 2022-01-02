9c229046a9d55fb67bfbe4013d239174f4efb84e
User <User@DESKTOP-98P96A5>
2022-01-02 09:35:09 +0530

    Lab524 command line argument problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab524.java
@@ -0,0 +1,13 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab524 {
+	public static void main(String[] args) {
+		if (args.length >= 2) {
+			String str1 = args[0];
+			String str2 = args[1];
+			System.out.println(str1 + str2);
+		} else {
+			System.out.println("Enter two int values as cla");
+		}
+	}
+}
