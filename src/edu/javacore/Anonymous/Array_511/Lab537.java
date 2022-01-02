13eb0f4b2cb8ac9d13d7149ce4785a47d2c551fd
User <User@DESKTOP-98P96A5>
2022-01-02 10:24:33 +0530

    Lab537

---
+++ src/edu/javacore/Anonymous/Array_511/Lab537.java
@@ -0,0 +1,21 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab537 {
+	public static void main(String[] args) {
+		int arr[][] = null;
+		arr = new int[3][];
+		int tmp[] = { 99, 88, 66, 77, 55 };
+		System.out.println("Len: " + arr.length);
+		arr[0] = new int[4];
+		arr[1] = new int[] { 10, 20, 30 };
+		arr[2] = tmp;
+		for (int ar[] : arr) {
+			for (int a : ar) {
+				System.out.println("\t" + a);
+				System.out.println();
+
+			}
+		}
+
+	}
+}
