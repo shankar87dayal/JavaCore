aea4aedbb42b25d8043093307981fc8f5e379f8d
User <User@DESKTOP-98P96A5>
2022-01-02 10:59:26 +0530

    Lab543

---
+++ src/edu/javacore/Anonymous/Array_511/Lab543.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab543 {
+public static void main(String[] args) {
+	int []ab,bc,cd[];
+	ab =new int[2];
+	bc=new int[7];
+	cd =new int[5][2]
+}
+}
