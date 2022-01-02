a7dbe482c1c51a09bfd0224294d226de6b5df0bd
User <User@DESKTOP-98P96A5>
2022-01-02 09:31:27 +0530

    Lab523 command line argument problem.

---
+++ src/edu/javacore/Anonymous/Array_511/Lab523.java
@@ -0,0 +1,12 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab523 {
+	public static void main(String[] args) {
+		System.out.println("Len: " + args.length);
+		System.out.println();
+		for (int i = 0; i < args.length; i++) {
+
+			System.out.println(i + "\t" + args[i]);
+		}
+	}
+}
