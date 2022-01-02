2e6307aa9ae7a1e7d567f0561371010413e9149c
User <User@DESKTOP-98P96A5>
2022-01-02 10:17:05 +0530

    Lab535

---
+++ src/edu/javacore/Anonymous/Array_511/Lab535.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab535 {
+	public static void main(String[] args) {
+		int arr[][] = null;
+		arr = new int[3][2];
+		arr[0][0] = 1234;
+		System.out.println(arr[0][0]);
+	}
+}
