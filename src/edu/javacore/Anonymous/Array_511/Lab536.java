398514bbb0e494bb2af04939a14c598642dca5b0
User <User@DESKTOP-98P96A5>
2022-01-02 10:19:43 +0530

    Lab536

---
+++ src/edu/javacore/Anonymous/Array_511/Lab536.java
@@ -0,0 +1,10 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab536 {
+	public static void main(String[] args) {
+		int arr[][] = null;
+		arr = new int[3][];
+		System.out.println("Len: " + arr.length);
+		System.out.println("Len: " + arr[0].length);
+	}
+}
