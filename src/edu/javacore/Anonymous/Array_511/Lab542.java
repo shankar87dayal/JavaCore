7a0993454e11d52a7e51389f9426848cd1073fda
User <User@DESKTOP-98P96A5>
2022-01-02 10:57:38 +0530

    Lab542

---
+++ src/edu/javacore/Anonymous/Array_511/Lab542.java
@@ -0,0 +1,13 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab542 {
+	public static void main(String[] args) {
+		int ab[], bc, cd;
+		ab = new int[2];
+		bc = 78;
+		cd = 89;
+		System.out.println(ab.length);
+		System.out.println(bc);
+		System.out.println(cd);
+	}
+}
