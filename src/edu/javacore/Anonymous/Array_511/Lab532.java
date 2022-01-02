498ac90064f6c27b10c687f7570f1af1cbb24e61
User <User@DESKTOP-98P96A5>
2022-01-02 10:09:20 +0530

    Lab532

---
+++ src/edu/javacore/Anonymous/Array_511/Lab532.java
@@ -0,0 +1,9 @@
+package edu.javacore.Anonymous.Array_511;
+
+public class Lab532 {
+	public static void main(String[] args) {
+		int arr[][] = null;
+		arr = new int[][]3;
+		System.out.println("L: " + arr.length);
+	}
+}
