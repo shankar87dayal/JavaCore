13989fd86a6ba78d098c478dea4c6ba19d83c87c
User <User@DESKTOP-98P96A5>
2022-01-04 07:03:13 +0530

    Lab264

---
+++ src/edu/javacore/Operater2/Pract/Lab264.java
@@ -0,0 +1,13 @@
+package edu.javacore.Operater2.Pract;
+
+class Hello3 {
+}
+
+public class Lab264 {
+	public static void main(String[] args) {
+		Hello3 h1 = null;
+		String str = null;
+		System.out.println(h1 instanceof Hello3);
+		System.out.println(str instanceof String);
+	}
+}
