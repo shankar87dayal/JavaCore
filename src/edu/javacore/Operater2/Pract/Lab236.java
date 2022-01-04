393017f739cce7672fa20073ccbc9d79dcd1c620
User <User@DESKTOP-98P96A5>
2022-01-04 06:55:01 +0530

    Lab236

---
+++ src/edu/javacore/Operater2/Pract/Lab236.java
@@ -0,0 +1,12 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab236 {
+	public static void main(String[] args) {
+		System.out.println(10 > 20);
+		System.out.println(10 < 20);
+		System.out.println(10 >= 10);
+		System.out.println(10 <= 10);
+		System.out.println(10 == 10);
+		System.out.println(10 == 12);
+	}
+}
