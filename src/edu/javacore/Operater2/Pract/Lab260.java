d15b40483b6daab993867e5976de9d6c56d97377
User <User@DESKTOP-98P96A5>
2022-01-04 07:02:11 +0530

    Lab260

---
+++ src/edu/javacore/Operater2/Pract/Lab260.java
@@ -0,0 +1,17 @@
+package edu.javacore.Operater2.Pract;
+
+class Hello {
+}
+
+class Hai {
+}
+
+public class Lab260 {
+
+	public static void main(String[] args) {
+		Hello h1 = new Hello();
+		Hai h2 = new Hai();
+		System.out.println(h1);
+		System.out.println(h2);
+	}
+}
