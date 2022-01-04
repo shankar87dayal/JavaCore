c280239ac4689870645510b4248cbbe8ddb63c91
User <User@DESKTOP-98P96A5>
2022-01-04 06:57:10 +0530

    Lab242

---
+++ src/edu/javacore/Operater2/Pract/Lab242.java
@@ -0,0 +1,8 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab242 {
+	public static void main(String[] args) {
+		System.out.println(10 / 3 == 10 / 3);
+		System.out.println(0.0 / 0 == 0.0 / 0);
+	}
+}
