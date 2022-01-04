3f471473111b72b044ab6c5368b4980997fa1f60
User <User@DESKTOP-98P96A5>
2022-01-04 07:05:40 +0530

    Lab273

---
+++ src/edu/javacore/Operater2/Pract/Lab273.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab273 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int min = a < b ? a : b;
+		System.out.println(min);
+	}
+}
