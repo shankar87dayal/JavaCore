8ecdca44edadfa110260fee6c0d8445e35d1ff86
User <User@DESKTOP-98P96A5>
2022-01-04 06:54:21 +0530

    lab234

---
+++ src/edu/javacore/Operater2/Pract/Lab234.java
@@ -0,0 +1,14 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab234 {
+	public static void main(String[] args) {
+		System.out.println('A' == 65);
+		byte b = 65;
+		System.out.println('A' == b);
+		System.out.println(65 == b);
+		System.out.println('A' == 65l);
+		System.out.println('A' == 65.0);
+		System.out.println('A' == 65f);
+
+	}
+}
