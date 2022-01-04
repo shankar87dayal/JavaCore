4588ac1b0711d1f950110c2dd4ae2365236cf237
User <User@DESKTOP-98P96A5>
2022-01-04 07:05:09 +0530

    Lab271

---
+++ src/edu/javacore/Operater2/Pract/Lab271.java
@@ -0,0 +1,9 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab271 {
+	public static void main(String[] args) {
+		Object obj = 10 > 20 ? 10 : "TWENTY";
+		System.out.println(obj);
+	}
+
+}
