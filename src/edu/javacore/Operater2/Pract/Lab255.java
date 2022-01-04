d7ce71ebf3118ca4cf68e97426c36ce80eb462ac
User <User@DESKTOP-98P96A5>
2022-01-04 07:00:49 +0530

    Lab255

---
+++ src/edu/javacore/Operater2/Pract/Lab255.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab255 {
+	public static void main(String[] args) {
+		int a = 12;
+		boolean b = a > 15 && a++ < 5;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+}
