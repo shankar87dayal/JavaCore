9ba6b57eb92a6f41f89abee3711013c424f35d36
User <User@DESKTOP-98P96A5>
2022-01-04 07:00:20 +0530

    Lab253

---
+++ src/edu/javacore/Operater2/Pract/Lab253.java
@@ -0,0 +1,13 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab253 {
+	public static void main(String[] args) {
+		int a = 99;
+		long b = 659874569;
+		boolean b1 = (a == 99) && (b == 659874569);
+		boolean b2 = (a == 99) && (b == 659874560);
+		System.out.println(b1);
+		System.out.println(b2);
+
+	}
+}
