14142f00ee5572851e6c42528ce7382b4b83295e
User <User@DESKTOP-98P96A5>
2022-01-04 06:54:42 +0530

    Lab235

---
+++ src/edu/javacore/Operater2/Pract/Lab235.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab235 {
+	public static void main(String[] args) {
+		double d1 = 13 / 3.0f;
+		double d2 = 13 / 3.0;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d1 == d2);
+	}
+}
