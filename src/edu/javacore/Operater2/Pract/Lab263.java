9b293885d55539af6ba76b63733436cabf65c108
User <User@DESKTOP-98P96A5>
2022-01-04 07:02:57 +0530

    Lab263

---
+++ src/edu/javacore/Operater2/Pract/Lab263.java
@@ -0,0 +1,13 @@
+package edu.javacore.Operater2.Pract;
+
+class Hello2 {
+}
+
+public class Lab263 {
+	public static void main(String[] args) {
+		Object obj = new Hello();
+		System.out.println(obj instanceof String);
+		System.out.println(obj instanceof Hello2);
+		System.out.println(obj instanceof Object);
+	}
+}
