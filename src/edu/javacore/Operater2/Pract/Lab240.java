5a0661e5ac16d8fc6a3e143bb75467837f6d4e6d
User <User@DESKTOP-98P96A5>
2022-01-04 06:56:25 +0530

    Lab240

---
+++ src/edu/javacore/Operater2/Pract/Lab240.java
@@ -0,0 +1,9 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab240 {
+	public static void main(String[] args) {
+		String str = null;
+		Hello h = null;
+		System.out.println(h == str);
+	}
+}
