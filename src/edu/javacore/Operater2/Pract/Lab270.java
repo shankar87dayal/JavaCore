424f29d37508ffc6e845dc619472448b44cc843f
User <User@DESKTOP-98P96A5>
2022-01-04 07:04:57 +0530

    Lab270

---
+++ src/edu/javacore/Operater2/Pract/Lab270.java
@@ -0,0 +1,11 @@
+package edu.javacore.Operater2.Pract;
+
+import javax.print.DocFlavor.STRING;
+
+public class Lab270 {
+	public static void main(String[] args) {
+		String str = 10 > 20 ? 10 : "TWENTY";
+		System.out.println(str);
+	}
+
+}
