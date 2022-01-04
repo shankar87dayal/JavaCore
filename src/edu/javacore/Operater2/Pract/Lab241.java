146419d51cbfe5c8b75a5a2ae5340dfac5761767
User <User@DESKTOP-98P96A5>
2022-01-04 06:56:38 +0530

    Lab241

---
+++ src/edu/javacore/Operater2/Pract/Lab241.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab241 {
+	public static void main(String[] args) {
+		String str = null;
+		Hello h = null;
+		Object ob = h;
+		System.out.println(ob == str);
+	}
+}
