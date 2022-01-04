bee09a0b84ef5f8b17a2b6c9c88fc0c2a5776d3a
User <User@DESKTOP-98P96A5>
2022-01-04 07:05:26 +0530

    Lab272

---
+++ src/edu/javacore/Operater2/Pract/Lab272.java
@@ -0,0 +1,10 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab272 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int max = a > b ? a : b;
+		System.out.println(max);
+	}
+}
