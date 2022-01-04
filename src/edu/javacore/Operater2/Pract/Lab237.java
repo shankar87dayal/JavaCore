f0d307b0f1b7d63f5372992ca33005fe18d527cf
User <User@DESKTOP-98P96A5>
2022-01-04 06:55:28 +0530

    Lab237

---
+++ src/edu/javacore/Operater2/Pract/Lab237.java
@@ -0,0 +1,9 @@
+package edu.javacore.Operater2.Pract;
+
+public class Lab237 {
+	public static void main(String[] args) {
+		System.out.println(true == true);
+		System.out.println(true == false);
+		System.out.println(true != false);
+	}
+}
