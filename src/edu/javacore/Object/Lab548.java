106121250264d97d4bb426e295b01f60c9dfe214
User <User@DESKTOP-98P96A5>
2022-01-08 23:38:49 +0530

    Lab548

---
+++ src/edu/javacore/Object/Lab548.java
@@ -0,0 +1,12 @@
+package edu.javacore.Object;
+
+public class Lab548 {
+	public static void main(String[] args) {
+		Hello3 h = null;
+		System.out.println(h.a);
+	}
+}
+
+class Hello3 {
+	int a;
+}
