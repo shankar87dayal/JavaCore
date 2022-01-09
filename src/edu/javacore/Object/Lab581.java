c0a86404dc0b6430b6e59fd288eedf4a38fb2138
User <User@DESKTOP-98P96A5>
2022-01-09 18:34:06 +0530

    Lab581

---
+++ src/edu/javacore/Object/Lab581.java
@@ -0,0 +1,20 @@
+package edu.javacore.Object;
+
+public class Lab581 {
+	public static void main(String[] args) {
+		Hello39 h = new Hello39();
+
+	}
+}
+
+class Hello39 {
+	int a;
+	static int b;
+	{
+		int c = 30;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
