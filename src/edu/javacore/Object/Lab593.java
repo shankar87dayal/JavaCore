8b22beaea3fbeff3abd07862bc146e3f21b187a3
User <User@DESKTOP-98P96A5>
2022-01-09 19:13:15 +0530

    Lab593

---
+++ src/edu/javacore/Object/Lab593.java
@@ -0,0 +1,10 @@
+package edu.javacore.Object;
+
+public class Lab593 {
+public static void main(String[] args) {
+	System.out.println("i am Main()");
+	{
+		System.out.println("i am Local Block in main()");
+	}
+}
+}
