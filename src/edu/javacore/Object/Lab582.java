c9dd734b44fea4b9a33e2b3f4aed13972ce77d25
User <User@DESKTOP-98P96A5>
2022-01-09 18:36:46 +0530

    Lab582  error:- The local variable c may not have been initialized

---
+++ src/edu/javacore/Object/Lab582.java
@@ -0,0 +1,19 @@
+package edu.javacore.Object;
+
+public class Lab582 {
+	public static void main(String[] args) {
+		Hello40 h = new Hello40();
+
+	}
+}
+
+class Hello40 {
+	int a;
+	static int b;
+	{
+		int c;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
