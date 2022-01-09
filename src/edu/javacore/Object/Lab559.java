f8d8c6f4af3d7ab213e9d3ca50ce9bc6a7ef0de2
User <User@DESKTOP-98P96A5>
2022-01-09 13:24:01 +0530

    Lab559

---
+++ src/edu/javacore/Object/Lab559.java
@@ -0,0 +1,11 @@
+package edu.javacore.Object;
+
+public class Lab559 {
+	public static void main(String[] args) {
+		System.out.println(Hello17.a);
+	}
+}
+
+class Hello17 {
+	static int a; a=10;
+}
