6c7e88083fdc626f1a8fcdf8210148d7f69c2cbf
User <User@DESKTOP-98P96A5>
2022-01-09 14:18:03 +0530

    Lab571

---
+++ src/edu/javacore/Object/Lab571.java
@@ -0,0 +1,11 @@
+package edu.javacore.Object;
+
+public class Lab571 {
+	public static void main(String[] args) {
+		System.out.println("main in Lab558");
+	}
+
+	static {
+		System.out.println("static Block in Lab558");
+	}
+}
