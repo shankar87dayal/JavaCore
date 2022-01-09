3a59cc2041a66fe2ef941bc169c70f27aa32a507
User <User@DESKTOP-98P96A5>
2022-01-09 18:23:56 +0530

    Lab578

---
+++ src/edu/javacore/Object/Lab578.java
@@ -0,0 +1,11 @@
+package edu.javacore.Object;
+
+public class Lab578 {
+	public static void main(String[] args) {
+		System.out.println("\n Main: " + Hello36.a);
+	}
+}
+
+class Hello36 {
+	static final int a=99;
+}
