c1e0286bac1aca745e24cc09ba0d1374257883ac
User <User@DESKTOP-98P96A5>
2022-01-09 13:08:50 +0530

    Lab554

---
+++ src/edu/javacore/Object/Lab554.java
@@ -0,0 +1,12 @@
+package edu.javacore.Object;
+
+public class Lab554 {
+	public static void main(String[] args) {
+		Hello14 h = null;
+		System.out.println(h.b);
+	}
+}
+
+class Hello14 {
+	static int b;
+}
