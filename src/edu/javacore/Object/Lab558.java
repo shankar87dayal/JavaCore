9d5ff997c4f2ee232e7d9f4f29781248ee6482b0
User <User@DESKTOP-98P96A5>
2022-01-09 13:21:42 +0530

    Lab558 Unresolved compilation problem:

---
+++ src/edu/javacore/Object/Lab558.java
@@ -0,0 +1,13 @@
+package edu.javacore.Object;
+
+public class Lab558 {
+	public static void main(String[] args) {
+		Hello16 h = new Hello16();
+		System.out.println(h.a);
+	}
+}
+
+class Hello16 {
+	int a;
+	a=10;
+}
