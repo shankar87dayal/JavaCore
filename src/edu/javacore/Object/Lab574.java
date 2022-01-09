036b8276bb514cd83d4ab6d2448ce702259ecd5b
User <User@DESKTOP-98P96A5>
2022-01-09 14:26:18 +0530

    Lab574

---
+++ src/edu/javacore/Object/Lab574.java
@@ -0,0 +1,12 @@
+package edu.javacore.Object;
+
+public class Lab574 {
+	public static void main(String[] args) {
+		Hello32 h = new Hello32();
+		System.out.println("\n main: " + h.a);
+	}
+}
+
+class Hello32 {
+	final int a;
+}
