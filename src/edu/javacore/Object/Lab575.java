bd4fbe91665ab8c58d0166264fb9dc4ec2c789f5
User <User@DESKTOP-98P96A5>
2022-01-09 18:16:08 +0530

    Lab575

---
+++ src/edu/javacore/Object/Lab575.java
@@ -0,0 +1,12 @@
+package edu.javacore.Object;
+
+public class Lab575 {
+	public static void main(String[] args) {
+		Hello33 h = new Hello33();
+		System.out.println("\nMain: " + h.a);
+	}
+}
+
+class Hello33 {
+	final int a = 99;
+}
