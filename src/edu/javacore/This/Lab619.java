58f67a20b26d21938ff991af4d0cb9078355b305
User <User@DESKTOP-98P96A5>
2022-01-10 11:47:05 +0530

    Lab619

---
+++ src/edu/javacore/This/Lab619.java
@@ -0,0 +1,24 @@
+package edu.javacore.This;
+
+public class Lab619 {
+	public static void main(String[] args) {
+		Student1 stu1 = new Student1(98,"Raushan");
+		stu1.show();
+		Student1 stu2 = new Student1(12,"Ravi");
+		stu2.show();
+	}
+}
+
+class Student1 {
+	int sid;
+	String sname;
+	Student1(int sid, String sname){
+		System.out.println("Student 2-arg constructor");
+		this.sid=sid;
+		this.sname =snname;
+	}
+
+	void show() {
+		System.out.println(sid + "\t" + sname);
+	}
+}
