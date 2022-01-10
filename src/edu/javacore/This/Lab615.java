f442bf02e17ec7df5d84ce3e237bb39cb8e75441
User <User@DESKTOP-98P96A5>
2022-01-10 11:33:39 +0530

    Lab615

---
+++ src/edu/javacore/This/Lab615.java
@@ -0,0 +1,18 @@
+package edu.javacore.This;
+
+public class Lab615 {
+	public static void main(String[] args) {
+		Hello1 h = new Hello1();
+		h.show();
+	}
+}
+
+class Hello1 {
+	int a = 10;
+
+	void show() {
+		String a = "Raushan";
+		System.out.println(a);
+		System.out.println(a);
+	}
+}
