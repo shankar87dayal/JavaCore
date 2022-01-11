7c3b44100a4b65b1934a7a39eac099f1b374ee87
User <User@DESKTOP-98P96A5>
2022-01-11 11:27:44 +0530

    Lab630

---
+++ src/edu/javacore/This/Lab630.java
@@ -0,0 +1,15 @@
+package edu.javacore.This;
+
+public class Lab630 {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+}
+
+class Hello14 {
+	static {
+		String a = "Raushan";
+		System.out.println("\n S.Block: " + a);
+		System.out.println(Hello14.a);
+	}
+}
