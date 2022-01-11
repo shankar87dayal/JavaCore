b845ecfbd4c037cf4b0a7f839525087134266961
User <User@DESKTOP-98P96A5>
2022-01-11 11:10:18 +0530

    Lab626

---
+++ src/edu/javacore/This/Lab626.java
@@ -0,0 +1,14 @@
+package edu.javacore.This;
+
+public class Lab626 {
+	public static void main(String[] args) {
+		Hello10 h = new Hello10();
+
+	}
+}
+
+class Hello10 {
+	Hello10() {
+		this();
+	}
+}
