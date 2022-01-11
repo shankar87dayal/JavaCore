2e2cf7eaaf04f56065813d6ee496569a7e6f9744
User <User@DESKTOP-98P96A5>
2022-01-11 17:26:09 +0530

    Lab678

---
+++ src/edu/javacore/This/Lab678.java
@@ -0,0 +1,15 @@
+package edu.javacore.This;
+
+public class Lab678 {
+	public static void main(String[] args) {
+		Happy7 h = new Happy7();
+		System.out.println(h.a);
+	}
+}
+
+class Happy7 {
+	 static final int a;
+	Heppy7(){
+		a = 90;
+	}
+}
