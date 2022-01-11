0741f059bd313593ad10a3740ab132f88b70756c
User <User@DESKTOP-98P96A5>
2022-01-11 17:11:54 +0530

    Lab671

---
+++ src/edu/javacore/This/Lab671.java
@@ -0,0 +1,12 @@
+package edu.javacore.This;
+
+public class Lab671 {
+	public static void main(String[] args) {
+		Happy h = new Happy();
+		System.out.println(h.a);
+	}
+}
+
+class Happy {
+	final int a;
+}
