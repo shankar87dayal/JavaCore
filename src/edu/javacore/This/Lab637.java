e8c9d7f3e6d7eb15f8cd3b018cff16ea8e2e8a37
User <User@DESKTOP-98P96A5>
2022-01-11 11:52:55 +0530

    Lab637

---
+++ src/edu/javacore/This/Lab637.java
@@ -0,0 +1,14 @@
+package edu.javacore.This;
+
+public class Lab637 {
+	public static void main(String[] args) {
+		System.out.println(a);
+		int a = 10;
+
+	}
+
+static {
+	System.out.println(b);
+	int b =20
+}
+}
