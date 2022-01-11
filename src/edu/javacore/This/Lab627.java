725c525570e7b6992329be4919f16aff05e19ee8
User <User@DESKTOP-98P96A5>
2022-01-11 11:15:37 +0530

    Lab627

---
+++ src/edu/javacore/This/Lab627.java
@@ -0,0 +1,18 @@
+package edu.javacore.This;
+
+public class Lab627 {
+	public static void main(String[] args) {
+		Hello11 h = new Hello11();
+
+	}
+}
+
+class Hello11 {
+	Hello11() {
+		this(10);
+	}
+
+	Hello11(int a) {
+		this();
+	}
+}
