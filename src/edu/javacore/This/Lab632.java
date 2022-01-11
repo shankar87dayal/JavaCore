b2903a3153918cc4c672add6d5c417d1de6fabf0
User <User@DESKTOP-98P96A5>
2022-01-11 11:32:03 +0530

    Lab632

---
+++ src/edu/javacore/This/Lab632.java
@@ -0,0 +1,14 @@
+package edu.javacore.This;
+
+public class Lab632 {
+	public static void main(String[] args) {
+     Hello15 h=new Hello15();
+     h.show();
+	}
+}
+
+class Hello15 {
+	void show() {
+		this =null;
+	}
+}
