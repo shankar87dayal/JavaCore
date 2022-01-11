448e39f53724e9102eba6a78869300a7d130ed12
User <User@DESKTOP-98P96A5>
2022-01-11 11:47:36 +0530

    Lab636

--- src/edu/javacore/This/Lab636.java
+++ src/edu/javacore/This/Lab636.java
@@ -1,21 +1,8 @@
 package edu.javacore.This;
 
-public class Lab636 {
-	public static void main(String[] args) {
-		Hello17 h = new Hello17();
-		h.show();
-
-	}
+   public class Lab636 {
+  public static void main(String[] args) {
+	Hello19 h =new Hello19();
+	h.show();
 }
-
-class Hello17 {
-	int a;
-
-	void show() {
-		String a = "Raushan";
-		System.out.println(a);
-		System.out.println(this.a);
-		System.out.println(Hello17.this.a);
-	}
-
 }
