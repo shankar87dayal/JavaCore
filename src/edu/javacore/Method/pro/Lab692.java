32174a7af5f2e5e45cb540f99c561d6661fbf2b5
User <User@DESKTOP-98P96A5>
2022-01-12 12:41:36 +0530

    Lab692

---
+++ src/edu/javacore/Method/pro/Lab692.java
@@ -0,0 +1,15 @@
+package edu.javacore.Method.pro;
+
+public class Lab692 {
+	public static void main(String[] args) {
+		Hello8.show();
+	}
+}
+
+class Hello8 {
+	static int b;
+
+   static void show() {
+		System.out.println(b);
+	}
+}
