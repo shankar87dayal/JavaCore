b07444964683e0bc4099bd5412ca7acb6eeff1f5
User <User@DESKTOP-98P96A5>
2022-01-12 12:39:02 +0530

    Lab685

---
+++ src/edu/javacore/Method/pro/Lab685.java
@@ -0,0 +1,13 @@
+package edu.javacore.Method.pro;
+
+public class Lab685 {
+	public static void main(String[] args) {
+		Hello1.show();
+	}
+}
+
+class Hello1 {
+	void show() {
+		System.out.println("show()");
+	}
+}
