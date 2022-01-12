ff523c21d6e4853484780792e93d57f2c9ea639d
User <User@DESKTOP-98P96A5>
2022-01-12 15:06:46 +0530

    Lab708

---
+++ src/edu/javacore/Method/pro/Lab708.java
@@ -0,0 +1,16 @@
+package edu.javacore.Method.pro;
+
+public class Lab708 {
+	public static void main(String[] args) {
+		Hello20 h = new Hello20();
+		System.out.println(h.show(10));
+	}
+}
+
+class Hello20 {
+	boolean show(int x) {
+		System.out.println("show() ");
+		return false;
+		return false;
+	}
+}
