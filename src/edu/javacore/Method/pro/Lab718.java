fa909ea85b5cc223d89353cd57ff8c8b638173bf
User <User@DESKTOP-98P96A5>
2022-01-12 15:42:30 +0530

    Lab718

---
+++ src/edu/javacore/Method/pro/Lab718.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro;
+
+public class Lab718 {
+	public static void main(String[] args) {
+		Hello30 h = new Hello30();
+		h.show((char)65);
+	}
+}
+
+class Hello30 {
+	void show(char x) {
+		System.out.println("show(char)");
+	}
+}
