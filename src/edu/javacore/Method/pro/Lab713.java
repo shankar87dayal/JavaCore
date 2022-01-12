3ef2d4e2fd5badacffad46d60dcb142effc48672
User <User@DESKTOP-98P96A5>
2022-01-12 15:30:19 +0530

    Lab713

---
+++ src/edu/javacore/Method/pro/Lab713.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro;
+
+public class Lab713 {
+	public static void main(String[] args) {
+		Hello25 h = new Hello25();
+		h.show(12);
+	}
+}
+
+class Hello25 {
+	void show(byte x) {
+		System.out.println("show(byte)");
+	}
+}
