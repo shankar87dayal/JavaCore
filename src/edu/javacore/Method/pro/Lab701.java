99544e6d44a94daaf4edfb63d2979dacc699fd79
User <User@DESKTOP-98P96A5>
2022-01-12 14:43:09 +0530

    Lab701

---
+++ src/edu/javacore/Method/pro/Lab701.java
@@ -0,0 +1,16 @@
+package edu.javacore.Method.pro;
+
+public class Lab701 {
+	public static void main(String[] args) {
+		Hello13 h = new Hello13();
+		h.show(10);
+		System.out.println("Hello guys");
+	}
+}
+
+class Hello13 {
+	int show(int x) {
+		System.out.println("show() ");
+	}
+
+}
