682e25cf4a576f051675bda34900277d2d421dad
User <User@DESKTOP-98P96A5>
2022-01-12 12:40:16 +0530

    Lab687

---
+++ src/edu/javacore/Method/pro/Lab688.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro;
+
+public class Lab688 {
+	public static void main(String[] args) {
+		Hello4 h = null;
+		h.show();
+	}
+}
+
+class Hello4 {
+	static void show() {
+		System.out.println("show()");
+	}
+}
