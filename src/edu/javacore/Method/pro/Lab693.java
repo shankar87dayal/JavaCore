b04e4cd6fc7f6fc5cb883b7050d75fbb167e6215
User <User@DESKTOP-98P96A5>
2022-01-12 12:59:12 +0530

    Lab693

---
+++ src/edu/javacore/Method/pro/Lab693.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro;
+
+public class Lab693 {
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.show();
+	}
+}
+
+class Student {
+	show(){
+		System.out.println("show()");
+	}
+}
