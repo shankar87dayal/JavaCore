a4fd922aa1d70c202821418daf1f990358558171
User <User@DESKTOP-98P96A5>
2022-01-15 00:19:33 +0530

    Lab758

---
+++ src/edu/javacore/Method/pro1/Lab758.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro1;
+
+public class Lab758 {
+	public static void main(String[] args) {
+		Hello13 h = new Hello13();
+		h.show(10);
+	}
+}
+
+class Hello13 {
+	void show(int... arr1,int...arr1) {
+		System.out.println("\nshow(int...,int...)");
+	}
+}
