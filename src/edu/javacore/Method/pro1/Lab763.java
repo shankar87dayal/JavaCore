3cb1866ca382c9df9e14eebe4529a048c5667b22
User <User@DESKTOP-98P96A5>
2022-01-15 00:33:11 +0530

    Lab63

---
+++ src/edu/javacore/Method/pro1/Lab763.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro1;
+
+public class Lab763 {
+	public static void main(String[] args) {
+		Hello18 h = new Hello18();
+		h.show( 10);
+	}
+}
+
+class Hello18 {
+	void show( int[]... arr) {
+		System.out.println("\nshow(int[]...)");
+	}
+}
