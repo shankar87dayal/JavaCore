c73a7729fb94f0538ef80bc65e9f07b78bb9fc84
User <User@DESKTOP-98P96A5>
2022-01-15 00:46:47 +0530

    La767

---
+++ src/edu/javacore/Method/pro1/Lab767.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro1;
+
+public class Lab767 {
+	public static void main(String[] args) {
+		Hello22 h = new Hello22();
+		h.show(10, 20);
+	}
+}
+
+class Hello22 {
+	void show(int a, int b) {
+		System.out.println("show(int,int)");
+	}
+}
