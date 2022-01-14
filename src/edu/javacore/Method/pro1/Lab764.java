434d6607f1005eb6bfec33a961d930b45d156d98
User <User@DESKTOP-98P96A5>
2022-01-15 00:34:29 +0530

    Lab764

---
+++ src/edu/javacore/Method/pro1/Lab764.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro1;
+
+public class Lab764 {
+	public static void main(String[] args) {
+		Hello19 h = new Hello19();
+		h.show();
+	}
+}
+
+class Hello19 {
+	void show( int[]... arr) {
+		System.out.println("\nshow(int[]...)");
+	}
+}
