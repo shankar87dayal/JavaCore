c20c62b570e00227ff0e5442501e3607f9688556
User <User@DESKTOP-98P96A5>
2022-01-20 09:47:44 +0530

    Lab805

---
+++ src/edu/javacore/Inheritance/Lab805.java
@@ -0,0 +1,12 @@
+package edu.javacore.Inheritance;
+
+public class Lab805 {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+}
+
+class Haii {
+}
+
+class Helloo extends Object,Haii{}
