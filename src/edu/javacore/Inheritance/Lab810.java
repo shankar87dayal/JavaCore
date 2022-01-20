e63de433fc20e985d1fb24900cd3a29fff04b48c
User <User@DESKTOP-98P96A5>
2022-01-20 10:05:10 +0530

    Lab810

---
+++ src/edu/javacore/Inheritance/Lab810.java
@@ -0,0 +1,13 @@
+package edu.javacore.Inheritance;
+
+public class Lab810 {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+}
+
+final class E1 {
+}
+
+class F1 extends E1 {
+}
