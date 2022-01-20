38f9925b5979a0a98ff33f4e73ca5f2868c971e6
User <User@DESKTOP-98P96A5>
2022-01-20 10:02:57 +0530

    Lab809

---
+++ src/edu/javacore/Inheritance/Lab809.java
@@ -0,0 +1,13 @@
+package edu.javacore.Inheritance;
+
+public class Lab809 {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+}
+
+class E extends F {
+}
+
+class F extends E { 
+}
