5d55c0d7fe70bfcf5f56249da6374d9a7af688b0
User <User@DESKTOP-98P96A5>
2022-01-20 09:45:32 +0530

    Lab804

---
+++ src/edu/javacore/Inheritance/Lab804.java
@@ -0,0 +1,15 @@
+package edu.javacore.Inheritance;
+
+public class Lab804 {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+}
+
+class A2 {
+}
+
+class B2 {
+}
+
+class C2 extends A2,B2{}
