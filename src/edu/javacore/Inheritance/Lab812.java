e89a0d0af0a48544328294c91eb9e81398d9748b
User <User@DESKTOP-98P96A5>
2022-01-20 10:23:53 +0530

    Lab812

--- src/edu/javacore/Inheritance/Lab812.java
+++ src/edu/javacore/Inheritance/Lab812.java
@@ -6,3 +6,15 @@ public class Lab812 {
 		hello.show();
 	}
 }
+class Haii5{
+	int b = 99;
+	protected int c=77;
+	public int d=66;
+}
+class Helloo5 extends Haii5{
+	void show() {
+		System.out.println(b);
+        System.out.println(c); 
+		System.out.println(d);
+	}
+}
