1590a7be8077fd2bbf6567c957dc839f6bcabcfa
User <User@DESKTOP-98P96A5>
2022-01-22 07:44:54 +0530

    Lab831

--- src/edu/javacore/Inheritance/Lab831.java
+++ src/edu/javacore/Inheritance/Lab831.java
@@ -2,19 +2,18 @@ package edu.javacore.Inheritance;
 
 public class Lab831 {
 	public static void main(String[] args) {
-		Hello24 hello = new Hello24();
 		hello.show();
 	}
 }
 
 class Hai24 {
-	static int a =10;
+	static int a = 10;
 }
 
 class Hello24 extends Hai24 {
- static	int a = 20;
+	static int a = 20;
 
-  static void show() {
+	static void show() {
 		int a = 30;
 		System.out.println(a);
 		System.out.println(this.a);
