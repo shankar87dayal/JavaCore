409be8862c96b97efb127849bde39ca5ce08500a
User <User@DESKTOP-98P96A5>
2022-01-25 11:30:28 +0530

    Lab894

---
+++ src/Acc/p1/Lab894.java
@@ -0,0 +1,24 @@
+package Acc.p1;
+
+import edu.javacore.Access_Modifier.*;
+import edu.javacore.Access_Modifier1.*;
+
+public class Lab894 {
+	public static void main(String[] args) {
+		A1 obj1 = new A1();
+		obj1.showA1();
+
+		B1 obj2 = new B1();
+		obj2.showB1();
+
+		C1 obj3 = new C1();
+		obj3.showC1();
+
+		D1 obj4 = new D1();
+		obj4.showD1();
+
+		E1 obj5 = new E1();
+		obj5.showE1();
+
+	}
+}
