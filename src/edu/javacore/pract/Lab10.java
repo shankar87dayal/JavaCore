c78e89e637705ddba857e94f81a34b9d7a7f94b2
User <User@DESKTOP-98P96A5>
2022-01-26 12:36:30 +0530

    class Lab10

---
+++ src/edu/javacore/pract/Lab10.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab10 {
+
+	public static void main(String[] args) {
+     int a, byte b;
+     System.out.println("value of a  is " + a + " b =" + b);
+	}
+
+}
