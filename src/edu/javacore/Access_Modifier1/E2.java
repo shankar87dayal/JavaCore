f3c439df6c77f7883e79dc78763df0c2e47c0144
User <User@DESKTOP-98P96A5>
2022-01-26 11:55:58 +0530

    class E2 from Access_Modifier

---
+++ src/edu/javacore/Access_Modifier1/E2.java
@@ -0,0 +1,16 @@
+package edu.javacore.Access_Modifier1;
+
+import edu.javacore.Access_Modifier.A2;
+
+public class E2 {
+	public void showE2() {
+		System.out.println("D->show()");
+		A2 ob1 = new A2();
+		System.out.println("#A2- Object#");
+//	System.out.println(ob1.a);
+//	System.out.println(ob1.b);
+//	System.out.println(ob1.c);
+		System.out.println(ob1.d);
+
+	}
+}
