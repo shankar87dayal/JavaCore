bf4844f987250ba8fa67b45ddf29f8a9416492b7
User <User@DESKTOP-98P96A5>
2022-01-26 12:00:55 +0530

    class Hai3 from Package

---
+++ src/edu/javacore/Package/Hai3.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package;
+
+public class Hai3 {
+	public static void m2() {
+		System.out.println("Hai3->m1()");
+	}
+	public static void show() {
+		System.out.println("Hai3->show()");
+	}
+}
