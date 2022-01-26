69f17fa91fc8c3b29ed75f30d8bc88a1060069d7
User <User@DESKTOP-98P96A5>
2022-01-26 12:01:42 +0530

    class Hello_1 from Package

---
+++ src/edu/javacore/Package/Hello_1.java
@@ -0,0 +1,12 @@
+package edu.javacore.Package;
+
+public class Hello_1 {
+void show() {
+	System.out.println("Hello -> Show()");
+}
+}
+class Hai_1{
+	void Display() {
+		System.out.println("Hai-> Diplay()");
+	}
+}
