c544a054005cdf66c438e9886f79cd17fb73a39c
User <User@DESKTOP-98P96A5>
2022-01-26 12:16:02 +0530

    class Lab882 from Package

---
+++ src/edu/javacore/Package1/Lab882.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package1;
+
+import static edu.javacore.Package.Hello3.show;
+import static edu.javacore.Package.Hai3.show;
+
+public class Lab882 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys");
+	}
+}
