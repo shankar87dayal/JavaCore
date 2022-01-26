df3c3d2fd0403643b51d765d7acdeb6c12a623fa
User <User@DESKTOP-98P96A5>
2022-01-26 11:55:05 +0530

    class D1 from Access_Modifier

---
+++ src/edu/javacore/Access_Modifier1/D1.java
@@ -0,0 +1,14 @@
+package edu.javacore.Access_Modifier1;
+
+import edu.javacore.Access_Modifier.A1;
+
+public class D1 extends A1 {
+	public void showD1() {
+		System.out.println("D->show()");
+		//System.out.println(a);
+		//System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+}
