78ca7cfdc6fdbc00903ee585f7d2a5df4b8f36f2
User <User@DESKTOP-98P96A5>
2022-01-26 12:31:52 +0530

    class Lab791 from Package

---
+++ src/edu/javacore/Method/pro1/Lab791.java
@@ -0,0 +1,13 @@
+package edu.javacore.Method.pro1;
+
+public class Lab791 {
+	public static void main(String args) {
+		System.out.println(args);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main(String[])");
+		main("Raushan");
+		main("Ranjan");
+	}
+}
