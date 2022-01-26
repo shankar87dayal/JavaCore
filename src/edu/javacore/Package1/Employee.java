d25e24520976b86c952853fbbe921607f12505cc
User <User@DESKTOP-98P96A5>
2022-01-26 12:10:34 +0530

    class Employee  from Package

---
+++ src/edu/javacore/Package1/Employee.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package1;
+
+public class Employee {
+	public int eid;
+	public String ename;
+	public void show() {
+		System.out.println("Employee -> show()");
+		System.out.println(eid + "\t" + ename);
+	}
+}
