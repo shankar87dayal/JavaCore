1c4a180b1eda7aa7a3f40e0c6cfa5b6a99570b09
User <User@DESKTOP-98P96A5>
2022-01-26 12:36:57 +0530

    class Lab12

---
+++ src/edu/javacore/pract/Lab12.java
@@ -0,0 +1,13 @@
+package edu.javacore.pract;
+
+public class Lab12 {
+
+	public static void main(String[] args) {
+		String str1, str2, str3;
+		str1 = str2 = str3 = "jlc";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
