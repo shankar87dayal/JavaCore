a98a143334efad05ec9fbd6de3a10ce3a41ea96b
User <User@DESKTOP-98P96A5>
2022-01-26 12:14:37 +0530

    class Lab878 from Package

---
+++ src/edu/javacore/Package1/Lab878.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package1;
+import static edu.javacore.Package.Hello2.m1;
+public class Lab878 {
+public static void main(String[] args) {
+	m1();
+}
+static void m1() {
+	System.out.println("Lab ->m1()");
+}
+}
