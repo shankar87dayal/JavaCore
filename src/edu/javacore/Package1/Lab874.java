f0980adb6fb798cc2a9208403fda7e617d3d17a5
User <User@DESKTOP-98P96A5>
2022-01-26 12:13:13 +0530

    class Lab874 from Package

---
+++ src/edu/javacore/Package1/Lab874.java
@@ -0,0 +1,13 @@
+package edu.javacore.Package1;
+
+import static edu.javacore.Package.Hai2.m3;
+import static edu.javacore.Package.Hello2.m1;
+import static edu.javacore.Package.Hello2.A;
+
+public class Lab874 {
+	public static void main(String[] args) {
+		System.out.println(A);
+		m1();
+		m3();
+	}
+}
