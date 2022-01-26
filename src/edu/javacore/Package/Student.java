4c2c1ee84f99af2c7ddce11a8eedb089d1312a9f
User <User@DESKTOP-98P96A5>
2022-01-26 12:33:58 +0530

    class Student from Package

---
+++ src/edu/javacore/Package/Student.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package;
+
+public class Student {
+public int sid;
+public String sname;
+public void show() {
+	System.out.println("student -> show()");
+	System.out.println(sid + "\t" +sname);
+}
+}
