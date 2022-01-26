6acc416a59caf60277e4d2a37af8423c9d83a2b6
User <User@DESKTOP-98P96A5>
2022-01-26 12:16:45 +0530

    class Lab885 from Package

---
+++ src/edu/javacore/Package1/Lab885.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package1;
+
+import java.lang.Math.*;
+
+public class Lab885 {
+	public static void main(String[] args) {
+		System.out.println(p1);
+		System.out.println(sqrt(16));
+	}
+}
