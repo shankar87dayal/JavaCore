e5c6f33edbe46c5ff71f9217938062108849ec45
User <User@DESKTOP-98P96A5>
2022-01-26 11:52:21 +0530

    class B2 Access_Modifier

---
+++ src/edu/javacore/Access_Modifier/B1.java
@@ -0,0 +1,13 @@
+package edu.javacore.Access_Modifier;
+
+public class B1 extends A1 {
+	public void showB1() {
+		System.out.println("B-> show()");
+		//System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
