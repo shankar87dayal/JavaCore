41808abe211dc63e46f89b72ef2d69fb5adf1b9f
User <User@DESKTOP-98P96A5>
2022-01-26 12:30:27 +0530

    class Lab787 from Package

---
+++ src/edu/javacore/Method/pro1/Lab787.java
@@ -0,0 +1,15 @@
+package edu.javacore.Method.pro1;
+
+public class Lab787 {
+	public static void main(String args) {
+		System.out.println(" main (String)");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(" main (String[])");
+	}
+
+	public  void main(int[] args) {
+		System.out.println(" main (int[])");
+	}
+}
