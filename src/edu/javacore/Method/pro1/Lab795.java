53dc4dfeb86192bd1f991e660ae404443400c73b
User <User@DESKTOP-98P96A5>
2022-01-26 12:32:43 +0530

    class Lab795 from Package

---
+++ src/edu/javacore/Method/pro1/Lab795.java
@@ -0,0 +1,13 @@
+package edu.javacore.Method.pro1;
+
+public class Lab795 {
+static {
+	System.out.println("Lab-S.B");
+	main(new String[] {"Sri","SD"});
+}
+public static void main(String... args) {
+     System.out.println("i am main(): "+ args.length);
+     for(String str: args)
+    	 System.out.println(str);
+}
+}
