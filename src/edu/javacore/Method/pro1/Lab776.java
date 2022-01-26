119d94441c5a5b797ae11345acc36cba4261dd1a
User <User@DESKTOP-98P96A5>
2022-01-26 12:27:14 +0530

    class Lab776 from Package

---
+++ src/edu/javacore/Method/pro1/Lab776.java
@@ -0,0 +1,11 @@
+package edu.javacore.Method.pro1;
+
+public class Lab776 {
+	static {
+		System.out.println(" i am S.B");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("i am main()");
+	}
+}
