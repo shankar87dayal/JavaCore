5de3c5e74f24ef7888826cdbf8bad7390530d4dd
User <User@DESKTOP-98P96A5>
2022-01-26 12:14:08 +0530

    class Lab876 from Package

---
+++ src/edu/javacore/Package1/Lab876.java
@@ -0,0 +1,12 @@
+package edu.javacore.Package1;
+
+import edu.javacore.Package.Hai2.*;
+import edu.javacore.Package.Hello2.*;
+
+public class Lab876 {
+public static void main(String[] args) {
+	System.out.println(B);
+	m2();
+	m4();
+}
+}
