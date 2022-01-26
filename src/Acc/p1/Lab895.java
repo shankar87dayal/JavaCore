130b9749e388a057de86341213ac5dd63c237b02
User <User@DESKTOP-98P96A5>
2022-01-26 11:58:09 +0530

    class Lab895 from Access_Modifier

---
+++ src/Acc/p1/Lab895.java
@@ -0,0 +1,25 @@
+package Acc.p1;
+
+import edu.javacore.Access_Modifier.*;
+import edu.javacore.Access_Modifier1.*;
+
+public class Lab895 {
+	public static void main(String[] args) {
+
+		A2 ob1 = new A2();
+		ob1.showA2();
+
+		B2 ob2 = new B2();
+		ob2.showA2();
+
+		C2 ob3 = new C2();
+		ob3.showC2();
+
+		D2 ob4 = new D2();
+		ob4.showD2();
+
+		E2 ob5 = new E2();
+		ob5.showE2();
+
+	}
+}
