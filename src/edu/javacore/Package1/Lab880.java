bffe8bd316587d30e8b0509c7609c99b76312f09
User <User@DESKTOP-98P96A5>
2022-01-26 12:15:24 +0530

    class Lab880 from Package

---
+++ src/edu/javacore/Package1/Lab880.java
@@ -0,0 +1,11 @@
+package edu.javacore.Package1;
+
+import static edu.javacore.Package.Hello3.*;
+import static edu.javacore.Package.Hai3.*;
+
+public class Lab880 {
+	public static void main(String[] args) {
+		m1();
+		m2();
+	}
+}
