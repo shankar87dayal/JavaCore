65d4fc90cf3c64ba24faa98e097dad93e9648695
User <User@DESKTOP-98P96A5>
2022-01-26 12:03:10 +0530

    class Hello3  from Package

---
+++ src/edu/javacore/Package/Hello3.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package;
+
+public class Hello3 {
+public static void m1() {
+	System.out.println("Hello3->m1()");
+}
+public static void show() {
+	System.out.println("Hello->show()");
+}
+}
