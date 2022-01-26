4342d1e89597e58e1654c8ad0ceb23f577781a7d
User <User@DESKTOP-98P96A5>
2022-01-26 12:32:18 +0530

    class Lab793 from Package

---
+++ src/edu/javacore/Method/pro1/Lab793.java
@@ -0,0 +1,15 @@
+package edu.javacore.Method.pro1;
+
+public class Lab793 {
+	{
+		System.out.println("Lab774-I.B");
+	}
+
+	Lab774(){
+		System.out.println("Lab774-D.B");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main (String[])");
+	}
+}
