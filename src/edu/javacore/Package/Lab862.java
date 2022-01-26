c2d52a4c879da523ce03f7b6098f1c8e898a5d2d
User <User@DESKTOP-98P96A5>
2022-01-26 12:06:56 +0530

    class Lab862  from Package

---
+++ src/edu/javacore/Package/Lab862.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package;
+
+public class Lab862 {
+	public static void main(String[] args) {
+		Hello_1 hobj = new Hello_1();
+		hobj.show();
+		Hai_1 hai = new Hai_1();
+		hai.Display();
+	}
+}
