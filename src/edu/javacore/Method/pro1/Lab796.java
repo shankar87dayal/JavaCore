c26e5ff2e376d36b96ffad50e69b11b257cd47a7
User <User@DESKTOP-98P96A5>
2022-01-26 12:32:56 +0530

    class Lab796 from Package

---
+++ src/edu/javacore/Method/pro1/Lab796.java
@@ -0,0 +1,14 @@
+package edu.javacore.Method.pro1;
+
+public class Lab796 {
+	{
+		System.out.println("Lab-S.B");
+		main(new String[] { "Sri", "SD" });
+	}
+
+	public static void main(String... args) {
+		System.out.println("i am main(): " + args.length);
+		for (String str : args)
+			System.out.println(str);
+	}
+}
