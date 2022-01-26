f48d4e8e599a528416670305a5512fba1758a27f
User <User@DESKTOP-98P96A5>
2022-01-26 12:20:22 +0530

    class Lab868 from Package

---
+++ src/edu/javacore/Package2/Lab868.java
@@ -0,0 +1,13 @@
+package edu.javacore.Package2;
+
+import edu.javacore.Package.*;
+import edu.javacore.Package1.*;
+
+public class Lab868 {
+	public static void main(String[] args) {
+		new A();
+		new B();
+		new X();
+		new Y();
+	}
+}
