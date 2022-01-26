5bad9cac3a508b48e0dcf15405ba02c061d24286
User <User@DESKTOP-98P96A5>
2022-01-26 12:37:11 +0530

    class Lab13

---
+++ src/edu/javacore/pract/Lab13.java
@@ -0,0 +1,9 @@
+package edu.javacore.pract;
+
+public class Lab13 {
+public static void main(String[] args) {
+		String str1 = "jlc", str2 = "chennai";
+		System.out.println(str1);
+		System.out.println(str2);
+}
+}
