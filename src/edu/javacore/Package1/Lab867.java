10276ff04f3f0ae24ddedb6b33d361d265eca872
User <User@DESKTOP-98P96A5>
2022-01-26 12:12:48 +0530

    class Lab867 from Package

---
+++ src/edu/javacore/Package1/Lab867.java
@@ -0,0 +1,10 @@
+package edu.javacore.Package1;
+
+import edu.javacore.Package.*;
+
+public class Lab867 {
+	public static void main(String[] args) {
+		Hello_2 hello = new Hello_2();
+		Hai_2 hai = null;
+	}
+}
