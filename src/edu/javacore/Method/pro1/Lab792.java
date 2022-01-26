2a3f9bdaf8cecaa57322210c8dc67b20ad4835b8
User <User@DESKTOP-98P96A5>
2022-01-26 12:32:06 +0530

    class Lab792 from Package

---
+++ src/edu/javacore/Method/pro1/Lab792.java
@@ -0,0 +1,13 @@
+package edu.javacore.Method.pro1;
+
+public class Lab792 {
+	public static void main(String[] args) {
+		System.out.println("Hello-main");
+	}
+}
+
+class Hai {
+	public static void main(String[] args) {
+		System.out.println("Hai-main");
+	}
+}
