c5d96f7500b0115e69ac5818150ec69659b63180
User <User@DESKTOP-98P96A5>
2022-01-26 11:55:43 +0530

    class E1 from Access_Modifier

---
+++ src/edu/javacore/Access_Modifier1/E1.java
@@ -0,0 +1,12 @@
+package edu.javacore.Access_Modifier1;
+import edu.javacore.Access_Modifier.A1;
+public class E1 {
+public void showE1() {
+	System.out.println("E->show()");
+	A1 a1 = new A1();
+//	//System.out.println(a1.a);
+//	System.out.println(a1.b);
+//	System.out.println(a1.c);
+	System.out.println(a1.d);
+}
+}
