bab83d16056b1fa815fef630c694e46349c8f3a2
User <User@DESKTOP-98P96A5>
2022-01-26 12:18:52 +0530

    class Lab891 from Package

---
+++ src/edu/javacore/Package1/Lab891.java
@@ -0,0 +1,11 @@
+package edu.javacore.Package1;
+
+public class Lab891 {
+	public static void main(java.lang.String[] args) {
+		java.lang.Object str = "Ravi";
+		System.out.println(str);
+	}
+}
+
+class Object {
+}
