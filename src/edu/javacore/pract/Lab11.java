b56cff89cf3bd954cc570f63b43c336582bfc2ed
User <User@DESKTOP-98P96A5>
2022-01-26 12:36:42 +0530

    class Lab11

---
+++ src/edu/javacore/pract/Lab11.java
@@ -0,0 +1,13 @@
+package edu.javacore.pract;
+
+public class Lab11 {
+
+	public static void main(String[] args) {
+    int a, b, c;
+    a=b=c = 99;
+    System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	}
+
+}
