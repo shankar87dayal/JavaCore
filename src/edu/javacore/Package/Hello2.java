79c93441f5b21d56b8b289b63f669c1e06258f86
User <User@DESKTOP-98P96A5>
2022-01-26 12:02:56 +0530

    class Hello2  from Package

---
+++ src/edu/javacore/Package/Hello2.java
@@ -0,0 +1,11 @@
+package edu.javacore.Package;
+
+public class Hello2 {
+public static int A=90;
+public static void m1() {
+	System.out.println("Hello2->m1()");
+}
+ static void m2() {
+	System.out.println("Hello2->m2()");
+}
+}
