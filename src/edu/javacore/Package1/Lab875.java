3077cc3ed12f8fcb39828545f262ccb6930d0cd8
User <User@DESKTOP-98P96A5>
2022-01-26 12:10:13 +0530

    class Lab875  from Package

---
+++ src/edu/javacore/Package1/Lab875.java
@@ -0,0 +1,12 @@
+package edu.javacore.Package1;
+
+import static edu.javacore.Package.Hai2.*;
+import static edu.javacore.Package.Hello2.*;
+
+public class Lab875 {
+	public static void main(String[] args) {
+		System.out.println(A);
+		m1();
+		m3();
+	}
+}
