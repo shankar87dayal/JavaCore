8abf636ec4985bfb8e51103a9140914affcc7591
User <User@DESKTOP-98P96A5>
2022-01-26 12:06:12 +0530

    class Lab859  from Package

---
+++ src/edu/javacore/Package/Lab859.java
@@ -0,0 +1,13 @@
+package edu.javacore.Package;
+
+public class Lab859 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+}
+
+class Hello2 {
+}
+
+class Hai {
+}
