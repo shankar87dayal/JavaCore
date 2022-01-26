b3285197a9b0c4c4972255039f964ec42831b672
User <User@DESKTOP-98P96A5>
2022-01-26 12:20:49 +0530

    class Lab870 from Package

---
+++ src/edu/javacore/Package2/Lab870.java
@@ -0,0 +1,8 @@
+package edu.javacore.Package2;
+import edu.javacore.Package.Hello1;
+import edu.javacore.Package1.Hello1;
+public class Lab870 {
+public static void main(String[] args) {
+	new Hello1();
+}
+}
