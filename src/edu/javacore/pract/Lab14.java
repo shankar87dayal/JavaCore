3f6a6761f4c036e510578ce97cc69337feacb1b7
User <User@DESKTOP-98P96A5>
2022-01-26 12:37:28 +0530

    class Lab14

---
+++ src/edu/javacore/pract/Lab14.java
@@ -0,0 +1,10 @@
+package edu.javacore.pract;
+
+public class Lab14 {
+public static void main(String[] args) {
+		int a = 120, b = 76, c = 56;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+}
+}
