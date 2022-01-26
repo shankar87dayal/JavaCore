c0da88ad73bfd87a6e39a15c7497dee6a7162106
User <User@DESKTOP-98P96A5>
2022-01-26 12:37:39 +0530

    class Lab15

---
+++ src/edu/javacore/pract/Lab15.java
@@ -0,0 +1,11 @@
+package edu.javacore.pract;
+
+public class Lab15 {
+public static void main(String[] args) {
+		String str1, str2;
+		String str3 = str1 = str2 = "jlc";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+}
+}
