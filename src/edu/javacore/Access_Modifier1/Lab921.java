4ad79f488ba22ae1fe19b67b3ce0afe6e66fb5b9
User <User@DESKTOP-98P96A5>
2022-01-27 10:16:35 +0530

    Class Lab921 from Acc-modifier

---
+++ src/edu/javacore/Access_Modifier1/Lab921.java
@@ -0,0 +1,17 @@
+package edu.javacore.Access_Modifier1;
+
+public class Lab921 {
+	public static void main(String[] args) {
+		new Bb4().m1();
+	}
+}
+
+class Aa4 {
+	public void m1() {
+	}
+}
+
+class Bb4 extends Aa4 {
+	public void m1() {
+	}
+}
