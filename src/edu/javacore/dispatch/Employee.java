a9988c06b54bbda981b7670ae1847d626848d336
User <User@DESKTOP-98P96A5>
2022-01-27 11:33:00 +0530

    class Employee from dispatch

---
+++ src/edu/javacore/dispatch/Employee.java
@@ -0,0 +1,15 @@
+package edu.javacore.dispatch;
+
+public class Employee {
+	void walking() {
+		System.out.println("Employee-> walking()");
+	}
+
+	void sleeping() {
+		System.out.println("Employee-> sleeping()");
+	}
+
+	static void working() {
+		System.out.println("Employee-> working()");
+	}
+}
