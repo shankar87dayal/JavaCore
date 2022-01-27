26bbcc99b82973956c3ce2c89be3d0ecc08c2e69
User <User@DESKTOP-98P96A5>
2022-01-27 13:19:36 +0530

    class Student from polymorphism

---
+++ src/edu/javacore/polymorphism/Student.java
@@ -0,0 +1,15 @@
+package edu.javacore.polymorphism;
+
+public class Student extends Person {
+	void walking() {
+		System.out.println("Student-> walking()");
+	}
+
+	static void sleeping() {
+		System.out.println("Student-> sleeping()");
+	}
+
+	void reading() {
+		System.out.println("Student->reading()");
+	}
+}
