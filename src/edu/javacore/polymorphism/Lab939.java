5dfd16a0937258ee3b08bc5bc8a989437798834f
User <User@DESKTOP-98P96A5>
2022-01-27 20:42:21 +0530

    class Lab939 from Polymorphism

---
+++ src/edu/javacore/polymorphism/Lab939.java
@@ -0,0 +1,22 @@
+package edu.javacore.polymorphism;
+
+public class Lab939 {
+	public static void main(String[] args) {
+		B4 bobj = new B4();
+		bobj.x = "Hello";
+		System.out.println(bobj.x);
+	}
+}
+
+class A4 {
+	int x = 10;
+}
+
+class B4 extends A4 {
+	String x = "Ravi";
+
+	void show() {
+		System.out.println(x);
+		System.out.println(super.x);
+	}
+}
