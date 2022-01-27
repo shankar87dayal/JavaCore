2041bf1159f17e32ed9c344b26d9726b2fa2530d
User <User@DESKTOP-98P96A5>
2022-01-27 13:13:34 +0530

    class Employee from polymorphism

---
+++ src/edu/javacore/polymorphism/Employee.java
@@ -0,0 +1,17 @@
+package edu.javacore.polymorphism;
+
+
+public class Employee extends Person {
+	void walking() {
+		System.out.println("Employee-> walking()");
+	}
+
+	static	void sleeping() {
+		System.out.println("Employee-> sleeping()");
+	}
+
+	 void working() {
+		System.out.println("Employee-> working()");
+	}
+}
+
