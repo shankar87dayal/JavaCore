df93362eb414ff4532d04081a40b1c6141a5876d
User <User@DESKTOP-98P96A5>
2022-01-27 13:18:01 +0530

    class Person from polymorphism

---
+++ src/edu/javacore/polymorphism/Person.java
@@ -0,0 +1,16 @@
+package edu.javacore.polymorphism;
+
+public class Person {
+	void eating() {
+		System.out.println("person-> eating()");
+	}
+
+	void walking() {
+		System.out.println("person-> walking()");
+	}
+
+	static void sleeping() {
+		System.out.println("person-> sleeping()");
+	}
+}
+
