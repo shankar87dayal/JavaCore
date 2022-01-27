f7740ca162cfb5159bddadaecc9188c1d50b109f
User <User@DESKTOP-98P96A5>
2022-01-27 11:32:34 +0530

    class Person from dispatch

---
+++ src/edu/javacore/dispatch/Person.java
@@ -0,0 +1,15 @@
+package edu.javacore.dispatch;
+
+public class Person {
+	void eating() {
+		System.out.println("person-> eating()");
+	}
+
+	void walking() {
+		System.out.println("person-> walking()");
+	}
+
+	static void sleeping() {
+		System.out.println("person-> sleeping()");
+	}
+}
