dbeec5c9120e3e6bf349ced12ccde5cb118fafda
User <User@DESKTOP-98P96A5>
2022-01-28 09:39:40 +0530

    class Student from Abstract class

---
+++ src/edu/javacore/polymorphism_1/Student.java
@@ -0,0 +1,9 @@
+package edu.javacore.polymorphism_1;
+import edu.javacore.polymorphism.*;
+
+public class Student extends Person12 {
+	void sleeping() {
+		System.out.println("Student -> sleeping()");
+	}
+
+}
