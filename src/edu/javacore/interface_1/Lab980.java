00274c3059bbfae0ed8b950173bf1ea21a54449d
User <User@DESKTOP-98P96A5>
2022-01-28 14:48:00 +0530

    class Lab980 from Interface

---
+++ src/edu/javacore/interface_1/Lab980.java
@@ -0,0 +1,11 @@
+package edu.javacore.interface_1;
+
+public class Lab980 {
+	public static void main(String[] args) {
+		A5 aobj = new D5();
+		System.out.println(aobj instanceof A5);
+		System.out.println(aobj instanceof B5);
+		System.out.println(aobj instanceof C5);
+		System.out.println(aobj instanceof D5);
+	}
+}
