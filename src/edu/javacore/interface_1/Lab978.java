ac3a8ff9bd73ebc63c859c03a37204f471a85cd4
User <User@DESKTOP-98P96A5>
2022-01-28 14:43:46 +0530

    class Lab978 from Interface

---
+++ src/edu/javacore/interface_1/Lab978.java
@@ -0,0 +1,11 @@
+package edu.javacore.interface_1;
+
+public class Lab978 {
+	public static void main(String[] args) {
+		A5 aobj = new D5();
+		if (aobj instanceof C5) {
+			C5 cobj = (C5) aobj;
+		}
+		System.out.println("Hello Guys!");
+	}
+}
