e1b03b57f281f39b3403709247b08fbd39b859e4
User <User@DESKTOP-98P96A5>
2022-01-28 14:45:23 +0530

    class Lab979 from Interface

---
+++ src/edu/javacore/interface_1/Lab979.java
@@ -0,0 +1,9 @@
+package edu.javacore.interface_1;
+
+public class Lab979 {
+	public static void main(String[] args) {
+		A5 aobj = new D5();
+		E5 eobj = (E) obj;
+		System.out.println("Hello Guys");
+	}
+}
