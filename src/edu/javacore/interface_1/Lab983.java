88017005ec24c302f246aef2f9c78b58cd80decb
User <User@DESKTOP-98P96A5>
2022-01-28 14:58:32 +0530

    class Lab983 from Interface

---
+++ src/edu/javacore/interface_1/Lab983.java
@@ -0,0 +1,12 @@
+package edu.javacore.interface_1;
+
+public class Lab983 {
+	public static void main(String[] args) {
+		A5 aobj = new D5();
+		E5 eboj = new E5();
+		System.out.println(aobj == eobj);
+		B5 bobj = new B5();
+		C5 cobj = new C5();
+		System.out.println(bobj == cobj);
+	}
+}
