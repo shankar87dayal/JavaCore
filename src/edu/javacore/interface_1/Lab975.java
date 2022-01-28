f008c7ee7cd7198a3af8d549a8696849e9e20aae
User <User@DESKTOP-98P96A5>
2022-01-28 13:37:41 +0530

    class Lab975 from Interface

---
+++ src/edu/javacore/interface_1/Lab975.java
@@ -0,0 +1,10 @@
+package edu.javacore.interface_1;
+
+public class Lab975 {
+	public static void main(String[] args) {
+		D5 dobj = new D5();
+		A5 aobj = dobj;
+		B5 bobj = aobj;
+		D5 dobj1 = aobj;
+	}
+}
