08246db3d79083cd640df856f3cb8072db333484
User <User@DESKTOP-98P96A5>
2022-01-28 11:48:03 +0530

    class Interr24 from Interface

---
+++ src/edu/javacore/interface_1/Interr24.java
@@ -0,0 +1,12 @@
+package edu.javacore.interface_1;
+
+public interface Interr24 {
+	Interr24(){}
+
+	{
+		System.out.println("Interr24 I.B");
+	}
+	static {
+		System.out.println("Interr24 S.B");
+	}
+}
