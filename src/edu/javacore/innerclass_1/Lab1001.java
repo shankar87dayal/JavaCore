aa5b73dce21e07d3ccf57af2f8c65f48a6619de8
User <User@DESKTOP-98P96A5>
2022-01-29 17:15:54 +0530

    class Lab1001 from Inner class

---
+++ src/edu/javacore/innerclass_1/Lab1001.java
@@ -0,0 +1,24 @@
+package edu.javacore.innerclass_1;
+
+class outer9 {
+	class Innerr_4 {
+	}
+
+	class Innerr_5 {
+	}
+
+	class Innerr_6 {
+	}
+
+	class Innerr_7 {
+	}
+
+	class Innerr_8 {
+	}
+}
+
+public class Lab1001 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+}
