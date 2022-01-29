fcf21fcbcc6f83f55f6cf46c89ba22100a7dab3f
User <User@DESKTOP-98P96A5>
2022-01-29 15:52:40 +0530

    class Lab995 from Inner class

---
+++ src/edu/javacore/innerclass_1/Lab995.java
@@ -0,0 +1,16 @@
+package edu.javacore.innerclass_1;
+
+class Outer3 {
+	class Inner3 {
+		static int a = 10;
+
+		static void m1() {
+		}
+	}
+}
+
+public class Lab995 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+}
