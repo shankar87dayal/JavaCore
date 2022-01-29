1659dee0a4727e56bf4ab3cf71009ae3d2a43882
User <User@DESKTOP-98P96A5>
2022-01-29 16:04:01 +0530

    class Lab999 from Inner class

---
+++ src/edu/javacore/innerclass_1/Lab999.java
@@ -0,0 +1,28 @@
+package edu.javacore.innerclass_1;
+
+class Outer7 {
+	private interface Inner_1 {
+	}
+
+	protected interface Inner_2 {
+	}
+
+	public interface Inner_3 {
+	}
+
+	static interface Inner_4 {
+	}
+
+	abstract interface Inner_5 {
+	}
+
+	private abstract interface Inner_6 {
+	}
+}
+
+public class Lab999 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+
+}
