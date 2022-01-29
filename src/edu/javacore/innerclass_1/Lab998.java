c9dc2f19258e8c73d39b9d21e91cd9426e8c39ca
User <User@DESKTOP-98P96A5>
2022-01-29 16:01:04 +0530

    class Lab998 from Inner class

---
+++ src/edu/javacore/innerclass_1/Lab998.java
@@ -0,0 +1,27 @@
+package edu.javacore.innerclass_1;
+
+class Outer6 {
+	private class Inner6 {
+	}
+
+	protected class Inner7 {
+	}
+
+	public class Inner8 {
+	}
+
+	static class Inner9 {
+	}
+
+	abstract class Inner10 {
+	}
+
+	private abstract class Inner11 {
+	}
+}
+
+public class Lab998 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+}
