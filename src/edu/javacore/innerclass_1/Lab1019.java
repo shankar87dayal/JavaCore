732737bea6d9e48214a859d4202993d919b7f234
User <User@DESKTOP-98P96A5>
2022-01-30 12:35:30 +0530

    class Lab1019 from inner class

---
+++ src/edu/javacore/innerclass_1/Lab1019.java
@@ -0,0 +1,11 @@
+package edu.javacore.innerclass_1;
+
+abstract class Person1 {
+	public abstract void sleeping();
+}
+
+public class Lab1019 {
+	public static void main(String[] args) {
+		Person1 p1 = new Person1();
+	}
+}
