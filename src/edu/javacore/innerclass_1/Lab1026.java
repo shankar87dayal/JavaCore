746a9781972eef8a7ce4bfd28e7ab15f9868d9ea
User <User@DESKTOP-98P96A5>
2022-01-30 13:03:16 +0530

    class Lab1026 from inner class

---
+++ src/edu/javacore/innerclass_1/Lab1026.java
@@ -0,0 +1,25 @@
+package edu.javacore.innerclass_1;
+
+import java.io.*;
+
+abstract class Hello_1 {
+
+};
+
+class Hai_1 extends Hello_1 {
+}
+
+interface Ball {
+}
+
+interface Shape {
+}
+
+public class Lab1026 {
+	public static void main(String[] args) {
+		// Hello_1 h1 = new Hello_1();
+		// Ball b1 = new Ball();
+		// Serializable s = new Serializable();
+		// Shape ss = new Shape();
+	}
+}
