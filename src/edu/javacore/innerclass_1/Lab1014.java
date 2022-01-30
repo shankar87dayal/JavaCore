53df78055193131e6ee7462c3ca46858839efdd7
User <User@DESKTOP-98P96A5>
2022-01-30 12:11:29 +0530

    class Lab1014 from inner class

---
+++ src/edu/javacore/innerclass_1/Lab1014.java
@@ -0,0 +1,24 @@
+package edu.javacore.innerclass_1;
+
+class OuterC6 {
+	void m1() {
+		class InnerC6 {
+		}
+	}
+
+	void m2() {
+		class Inner7 {
+		}
+	}
+
+	void m3() {
+		InnerC6 ob1 = new InnerC6();
+		InnerC7 ob2 = new InnerC7();
+	}
+}
+
+public class Lab1014 {
+	public static void main(String[] args) {
+		System.out.println("Hello Guys!");
+	}
+}
