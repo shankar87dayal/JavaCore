aee5c7d9f4b3a5ae850453626d5755cb42b4aa30
User <User@DESKTOP-98P96A5>
2022-02-02 12:45:19 +0530

    class Lab1043 from Deep Cloning

---
+++ src/com/javacore/package_1/Lab1043.java
@@ -0,0 +1,12 @@
+package com.javacore.package_1;
+
+public class Lab1043 {
+public static void main(String[] args) {
+	Student5 st1 =new Student5();
+	Student5 st2 = (Student5)st1.clone();
+	System.out.println(st1==st2);
+}
+}
+class Student5{
+	
+}
