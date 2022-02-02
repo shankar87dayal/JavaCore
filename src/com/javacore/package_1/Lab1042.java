7003d49d3de96b0a9850f3f0c4ea0687636caab8
User <User@DESKTOP-98P96A5>
2022-02-02 12:40:56 +0530

    class Lab1042 from Deep Cloning

---
+++ src/com/javacore/package_1/Lab1042.java
@@ -0,0 +1,9 @@
+package com.javacore.package_1;
+
+public class Lab1042 {
+	public static void main(String[] args) {
+		Object obj = new Object();
+		Object obj1 = obj.clone();
+		System.out.println(obj1 == obj);
+	}
+}
