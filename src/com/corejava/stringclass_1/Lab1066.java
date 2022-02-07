40e55d5788c3366463fd3020e78bb31adb202416
User <User@DESKTOP-98P96A5>
2022-02-07 20:31:20 +0530

    class Lab1066 from string class

---
+++ src/com/corejava/stringclass_1/Lab1066.java
@@ -0,0 +1,10 @@
+package com.corejava.stringclass_1;
+
+public class Lab1066 {
+	public static void main(String[] args) {
+		int ab = 98;
+		String st1 = ab;
+		String st2 = (String) ab;
+		System.out.println(st1 + "\t" + st2);
+	}
+}
