f12c00a5c2485badc3450529882f9937c6293845
User <User@DESKTOP-98P96A5>
2022-02-07 20:46:51 +0530

    class Lab1069 from string class

---
+++ src/com/corejava/stringclass_1/Lab1069.java
@@ -0,0 +1,12 @@
+package com.corejava.stringclass_1;
+
+public class Lab1069 {
+	public static void main(String[] args) {
+		Student stu = null;
+		String st = String.valueOf(stu);
+		System.out.println(st);
+	}
+}
+
+class Student {
+}
