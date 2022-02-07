ac426b4f94786d19f37d180c942ef0b2f6bd2515
User <User@DESKTOP-98P96A5>
2022-02-07 20:15:15 +0530

    class Lab1061 from string class

---
+++ src/com/corejava/stringclass_1/Lab1061.java
@@ -0,0 +1,10 @@
+package com.corejava.stringclass_1;
+
+public class Lab1061 {
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println("x=" + x == "x=" + x);
+		final int a = 10;
+		System.out.println("A=" + a == "A=" + a);
+	}
+}
