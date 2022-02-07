39c6a469af893a8914067082cd554f4c507a021f
User <User@DESKTOP-98P96A5>
2022-02-07 20:18:51 +0530

    class Lab1063 from string class

---
+++ src/com/corejava/stringclass_1/Lab1063.java
@@ -0,0 +1,9 @@
+package com.corejava.stringclass_1;
+
+public class Lab1063 {
+	public static void main(String[] args) {
+		int ab = 98;
+		int bc = 98;
+		System.out.println("Results is: " + (ab == bc));
+	}
+}
