7832c5b223ab9b0bf9a073b3bdd4e121e4ef8c88
User <User@DESKTOP-98P96A5>
2022-02-07 19:56:54 +0530

    class Lab1058 from string class

---
+++ src/com/corejava/stringclass_1/Lab1058.java
@@ -0,0 +1,12 @@
+package com.corejava.stringclass_1;
+
+public class Lab1058 {
+	public static void main(String[] args) {
+		String st1 = "JLC99";
+		String st2 = "JLC";
+		final int ab = 99;
+		String st3 = st2 + ab;
+		System.out.println(st1 + "\t" + st3);
+		System.out.println(st1 == st3);
+	}
+}
