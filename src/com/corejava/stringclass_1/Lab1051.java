fbcce64685beaba40eb7d00081a01952008e2f60
User <User@DESKTOP-98P96A5>
2022-02-07 13:48:55 +0530

    class Lab1051 form string class

---
+++ src/com/corejava/stringclass_1/Lab1051.java
@@ -0,0 +1,16 @@
+package com.corejava.stringclass_1;
+
+public class Lab1051 {
+	public static void main(String[] args) {
+		String st1 = "jlc";
+		String st2 = new String("jlc");
+		String st3 = new String("jlc");
+		System.out.println(st1);
+		System.out.println(st2);
+		System.out.println(st3);
+		System.out.println(st1 == st2);
+		System.out.println(st1 == st3);
+		System.out.println(st2 == st3);
+
+	}
+}
