d8f44a65d0ea7eababae33d25a42c4016e8ab6c0
User <User@DESKTOP-98P96A5>
2022-02-07 20:35:07 +0530

    class Lab1067 from string class

---
+++ src/com/corejava/stringclass_1/Lab1067.java
@@ -0,0 +1,10 @@
+package com.corejava.stringclass_1;
+
+public class Lab1067 {
+	public static void main(String[] args) {
+		int ab = 98;
+		String st1 = String.valueOf(true);
+		String st2 = String.valueOf(ab);
+		System.out.println(st1 + "\t" + st2);
+	}
+}
