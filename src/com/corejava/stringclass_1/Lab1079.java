8e30fcdea7c3aefa7b8fa22e5d7e64f7c91d10db
User <User@DESKTOP-98P96A5>
2022-02-08 16:26:51 +0530

    class Lab1079 from String class

---
+++ src/com/corejava/stringclass_1/Lab1079.java
@@ -0,0 +1,15 @@
+package com.corejava.stringclass_1;
+
+public class Lab1079 {
+	public static void main(String[] args) {
+		String str = "Hi, Welcome to JLC (java learning center),java world";
+		String str1 = str.replace('j', 'x');
+		System.out.println(str1);
+		String str2 = str.replace("java", "SD");
+		System.out.println(str2);
+		String str3 = str.replace("java", "SD");
+		System.out.println(str3);
+		System.out.println(str);
+
+	}
+}
