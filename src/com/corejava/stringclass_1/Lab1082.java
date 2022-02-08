edef7a3cfae5a52013da9e0d6a84f552b0c6a6b1
User <User@DESKTOP-98P96A5>
2022-02-08 16:37:10 +0530

    class Lab1082 from String class

---
+++ src/com/corejava/stringclass_1/Lab1082.java
@@ -0,0 +1,10 @@
+package com.corejava.stringclass_1;
+
+public class Lab1082 {
+	public static void main(String[] args) {
+		String str = "welcome to jlc, java training center , No1 in java training and replace";
+		System.out.println(str.indexOf("java"));
+		System.out.println(str.indexOf("java", 16));
+		System.out.println(str.indexOf("java", 17));
+	}
+}
