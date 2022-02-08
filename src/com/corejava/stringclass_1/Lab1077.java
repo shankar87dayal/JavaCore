a74d78c4da0ce78532cacaeaa9bd76d122e4838d
User <User@DESKTOP-98P96A5>
2022-02-08 16:12:16 +0530

    class Lab1077 from String class

---
+++ src/com/corejava/stringclass_1/Lab1077.java
@@ -0,0 +1,10 @@
+package com.corejava.stringclass_1;
+
+public class Lab1077 {
+	public static void main(String[] args) {
+		String str = "Hai, Welcome to Jlc";
+		System.out.println(str.startsWith("Hai"));
+		System.out.println(str.startsWith("Welcome"));
+		System.out.println(str.startsWith("Welcome", 5));
+	}
+}
