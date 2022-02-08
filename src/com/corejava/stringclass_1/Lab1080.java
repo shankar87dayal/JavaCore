089481db01d59ce11b1a96dea94de67be401a5a6
User <User@DESKTOP-98P96A5>
2022-02-08 16:33:04 +0530

    class Lab1081 from String class

---
+++ src/com/corejava/stringclass_1/Lab1080.java
@@ -0,0 +1,9 @@
+package com.corejava.stringclass_1;
+
+public class Lab1080 {
+	public static void main(String[] args) {
+		String str = "welcome to jlc, java training center, No1 in java training and replace";
+		System.out.println(str.indexOf(97));
+		System.out.println(str.indexOf('a'));
+	}
+}
---
+++ src/com/corejava/stringclass_1/Lab1081.java
@@ -0,0 +1,11 @@
+package com.corejava.stringclass_1;
+
+public class Lab1081 {
+	public static void main(String[] args) {
+		String str = "welcome to jlc, java training center, No1 in java training and replace";
+		System.out.println(str.indexOf('j'));
+		System.out.println(str.indexOf('j', 11));
+		System.out.println(str.indexOf('j', 12));
+		System.out.println(str.indexOf('j', 17));
+	}
+}
