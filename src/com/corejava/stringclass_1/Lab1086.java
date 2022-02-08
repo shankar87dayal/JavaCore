18034884efe52011c1fec07130850ac48aed8413
User <User@DESKTOP-98P96A5>
2022-02-08 16:54:31 +0530

    class Lab1086 from String class

---
+++ src/com/corejava/stringclass_1/Lab1086.java
@@ -0,0 +1,8 @@
+package com.corejava.stringclass_1;
+
+public class Lab1086 {
+	public static void main(String[] args) {
+		String str = "JLCINDIA";
+		System.out.println(str.substring(3, 9));
+	}
+}
