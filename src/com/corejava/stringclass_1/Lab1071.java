4917b93a4ea70bc1aabaeb4ece0a1c38a02c454e
User <User@DESKTOP-98P96A5>
2022-02-08 15:58:14 +0530

    class Lab1071 from String class

---
+++ src/com/corejava/stringclass_1/Lab1071.java
@@ -0,0 +1,13 @@
+package com.corejava.stringclass_1;
+
+public class Lab1071 {
+	public static void main(String[] args) {
+		String str = "jlcindia";
+		int len = str.length();
+		System.out.println(str + "\t" + len);
+		String str1 = str.toLowerCase();
+		String str2 = str.toUpperCase();
+		System.out.println(str1 + "\t" + str2);
+
+	}
+}
