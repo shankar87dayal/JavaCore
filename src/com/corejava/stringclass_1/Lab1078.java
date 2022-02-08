6bc0215cd2d80efbb10e2d0a26be86e92405827f
User <User@DESKTOP-98P96A5>
2022-02-08 16:16:05 +0530

    class Lab1078 from String class

---
+++ src/com/corejava/stringclass_1/Lab1078.java
@@ -0,0 +1,12 @@
+package com.corejava.stringclass_1;
+
+public class Lab1078 {
+	public static void main(String[] args) {
+		String str = "Hai, Welcome to Jlc";
+		System.out.println(str.endsWith("Jlc"));
+		System.out.println(str.endsWith("Welcome"));
+		System.out.println();
+		System.out.println(str.startsWith(""));
+		System.out.println(str.endsWith(""));
+	}
+}
