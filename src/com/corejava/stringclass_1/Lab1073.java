1d4947f91778b4ef4cfe31b0911e78d9be083c41
User <User@DESKTOP-98P96A5>
2022-02-08 16:04:07 +0530

    class Lab1073 from String class

---
+++ src/com/corejava/stringclass_1/Lab1073.java
@@ -0,0 +1,9 @@
+package com.corejava.stringclass_1;
+
+public class Lab1073 {
+	public static void main(String[] args) {
+		String str = "";
+		System.out.println(str.length());
+		System.out.println(str.isEmpty());
+	}
+}
