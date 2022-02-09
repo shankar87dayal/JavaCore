550a5b101579bc637ba2ff0449046e3725326321
User <User@DESKTOP-98P96A5>
2022-02-09 12:12:26 +0530

    class Lab1103 from String class

---
+++ src/com/corejava/stringclass_1/Lab1103.java
@@ -0,0 +1,14 @@
+package com.corejava.stringclass_1;
+
+public class Lab1103 {
+	public static void main(String[] args) {
+		String str = " jLC, java training center. No1 in java training and placement.java is a popular language.";
+		System.out.println(str);
+		String dir = "D:\\B01\\abc\\java\\abc.txt";
+		String res[] = dir.split("\\\\");
+		for (int i = 0; i < res.length; i++) {
+			String st = res[i];
+			System.out.println(i + "\t" + st);
+		}
+	}
+}
