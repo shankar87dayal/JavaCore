d6bea0582d0a96587872c0699f4e78f71ffcd57f
User <User@DESKTOP-98P96A5>
2022-02-09 10:05:14 +0530

    class Lab1096 from String class

---
+++ src/com/corejava/stringclass_1/Lab1096.java
@@ -0,0 +1,12 @@
+package com.corejava.stringclass_1;
+
+public class Lab1096 {
+	public static void main(String[] args) {
+		char cArr[] = { 'J', 'L', 'C', 'I', 'N', 'D', 'I', 'A' };
+		byte bArr[] = { 65, 66, 67, 68, 69, 98, 97, 50 };
+		String st1 = new String(cArr);
+		String st2 = new String(bArr);
+		System.out.println("st1: " + st1);
+		System.out.println("st2: " + st2);
+	}
+}
