22d0515331a98bab15daaa95a6a2a4b26c861483
User <User@DESKTOP-98P96A5>
2022-02-11 10:38:42 +0530

    class Lab1130 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1130.java
@@ -0,0 +1,10 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1130 {
+	public static void main(String[] args) {
+		String str = "A";
+		StringBuilder sb = new StringBuilder("JLC");
+		System.out.println(str.hashCode());
+		System.out.println(sb.hashCode());
+	}
+}
