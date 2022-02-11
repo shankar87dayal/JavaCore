0efcf081897dc80ee67ca83efca0a89eaf631369
User <User@DESKTOP-98P96A5>
2022-02-11 10:39:01 +0530

    class Lab1131 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1131.java
@@ -0,0 +1,9 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1131 {
+	public static void main(String[] args) {
+		StringBuilder sb1 = new StringBuilder("A");
+		StringBuilder sb2 = new StringBuilder("A");
+		System.out.println(sb1.equals(sb2));
+	}
+}
