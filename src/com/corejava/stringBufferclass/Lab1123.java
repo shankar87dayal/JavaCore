4bad05edafecaf703c51be7be3c71e8f6649a270
User <User@DESKTOP-98P96A5>
2022-02-11 10:10:32 +0530

    class Lab1123 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1123.java
@@ -0,0 +1,12 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1123 {
+	public static void main(String[] args) {
+		StringBuilder sb1 = new StringBuilder("JLCINDIA");
+		System.out.println("\nC: " + sb1.capacity());
+		System.out.println("L: " + sb1.length());
+		sb1.ensureCapacity(9060088065);
+		System.out.println("\nC: " + sb1.capacity());
+		System.out.println("L: " + sb1.length());
+	}
+}
