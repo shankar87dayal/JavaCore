2e6844011eae116d141f821af8d3a62c466f04a6
User <User@DESKTOP-98P96A5>
2022-02-11 10:11:08 +0530

    class Lab1124 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1124.java
@@ -0,0 +1,8 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1124 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder(null);
+		System.out.println(sb);
+	}
+}
