b036c5e6da523b13d00471e69093e7e1ed9fbab4
User <User@DESKTOP-98P96A5>
2022-02-11 10:12:09 +0530

    class Lab1128 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1128.java
@@ -0,0 +1,9 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1128 {
+	public static void main(String[] args) {
+		String str = "JLC";
+		StringBuilder sb = new StringBuilder("JLC");
+		System.out.println(str.equals(sb));
+	}
+}
