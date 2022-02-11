74d8421b64d02879fdbbdd0bd7692ad4d110891d
User <User@DESKTOP-98P96A5>
2022-02-11 10:11:27 +0530

    class Lab1125 from stringBufferclass

---
+++ src/com/corejava/stringBufferclass/Lab1125.java
@@ -0,0 +1,9 @@
+package com.corejava.stringBufferclass;
+
+public class Lab1125 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(null);
+		System.out.println(sb);
+	}
+}
