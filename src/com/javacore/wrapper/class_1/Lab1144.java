b9e310539c5cce4532297260fc97baa79cf95d25
User <User@DESKTOP-98P96A5>
2022-02-14 14:45:57 +0530

    class Lab1144 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1144.java
@@ -0,0 +1,9 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1144 {
+public static void main(String[] args) {
+	short b1 = short.parseShort("101",2);
+	System.out.println(b1);
+	short bref = short.v
+}
+}
