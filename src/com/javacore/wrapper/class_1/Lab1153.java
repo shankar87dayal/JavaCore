65b7c4118f62b365dad1d3ea5572bff2f2ff7963
User <User@DESKTOP-98P96A5>
2022-02-14 14:48:41 +0530

    class Lab1153 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1153.java
@@ -0,0 +1,10 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1153 {
+	public static void main(String[] args) {
+		String str = Long.toBinaryString(65);
+		System.out.println(str);
+		System.out.println(Long.toHexString(65));
+		System.out.println(Long.toOctalString(65));
+	}
+}
