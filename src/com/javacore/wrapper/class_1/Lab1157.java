6df8fa71a34080b9695ec197f869ec4c771d6d4c
User <User@DESKTOP-98P96A5>
2022-02-14 14:49:46 +0530

    class Lab1157 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1157.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1157 {
+	public static void main(String[] args) {
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.SIZE);
+		System.out.println(Float.POSITIVE_INFINITY);
+		System.out.println(Float.NEGATIVE_INFINITY);
+		System.out.println(Float.NaN);
+	}
+}
