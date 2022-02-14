48e666912f4f3be7411a3bb966b28c4c91442331
User <User@DESKTOP-98P96A5>
2022-02-14 14:50:19 +0530

    class Lab1159 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1159.java
@@ -0,0 +1,10 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1159 {
+	public static void main(String[] args) {
+//	System.out.println(Float.parseFloat("101",2));
+//	System.out.println(Float.parseFloat("101",8));
+//	System.out.println(Float.valueOf("101",2));
+//	System.out.println(Float.valueOf("101",16));
+	}
+}
