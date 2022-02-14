0c8ba11f64d5d2dc820ee6bb77ce0f32d5d7cd40
User <User@DESKTOP-98P96A5>
2022-02-14 14:42:05 +0530

    class Lab1134 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1134.java
@@ -0,0 +1,14 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1134 {
+	public static void main(String[] args) {
+		String str = "99";
+		int a = Integer.parseInt(str);
+		byte b1 = Byte.parseByte("88");
+		String s1 = "A";
+		char ch = s1.charAt(0);
+//	byte b2 = Byte.parseByte("129");NFE
+//	int x = Integer.parseInt("65734687787043");NFE
+//	int y = Integer.parseInt("123.0");NFE
+	}
+}
