6ee87ee696b062bd267017d303e81b1f594217be
User <User@DESKTOP-98P96A5>
2022-02-14 14:45:20 +0530

    class Lab1143 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1143.java
@@ -0,0 +1,9 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1143 {
+	public static void main(String[] args) {
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.SIZE);
+	}
+}
