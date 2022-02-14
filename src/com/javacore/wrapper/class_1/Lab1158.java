c312ee1cfa2898410932524b82c82d966a697c43
User <User@DESKTOP-98P96A5>
2022-02-14 14:50:01 +0530

    class Lab1158 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1158.java
@@ -0,0 +1,11 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1158 {
+	public static void main(String[] args) {
+//	String str = Float.toBinaryString(65.0F);
+//	System.out.println(Float.toOctalString(65.0f));
+		System.out.println(Float.toHexString(16.0f));
+		System.out.println(Float.toHexString(32.0f));
+		System.out.println(Float.toHexString(0.0f));
+	}
+}
