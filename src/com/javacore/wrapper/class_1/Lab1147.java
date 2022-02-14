b313a196b952b4d89c038baa0e4be39a87f6b331
User <User@DESKTOP-98P96A5>
2022-02-14 14:46:58 +0530

    class Lab1147 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1147.java
@@ -0,0 +1,9 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1147 {
+	public static void main(String[] args) {
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.SIZE);
+	}
+}
