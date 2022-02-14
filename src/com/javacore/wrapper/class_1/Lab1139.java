b2e0c555cdcf036810d2938e54fe507267fdd1e5
User <User@DESKTOP-98P96A5>
2022-02-14 14:43:47 +0530

    class Lab1139 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1139.java
@@ -0,0 +1,9 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1139 {
+	public static void main(String[] args) {
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.SIZE);
+	}
+}
