3803c3bdf80fc3734d3558e9a8ddd509635d568a
User <User@DESKTOP-98P96A5>
2022-02-14 14:43:29 +0530

    class Lab1138 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1138.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1138 {
+	public static void main(String[] args) {
+		Integer in = new Integer(1234);
+		String s1 = in.toString();
+		Boolean b1 = new Boolean("JLC");
+		String s2 = b1.toString();
+		Character ch = new Character("A");
+		String s3 = ch.toString();
+	}
+}
