5ae76db2c37613449ab8e5bddd919bab14d4e86a
User <User@DESKTOP-98P96A5>
2022-02-14 14:42:25 +0530

    class Lab1135 from wrapperclass

---
+++ src/com/javacore/wrapper/class_1/Lab1135.java
@@ -0,0 +1,16 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1135 {
+	public static void main(String[] args) {
+		int a = 10;
+		Integer in = new Integer(a);
+		System.out.println(in);
+		byte b1 = 123;
+		Byte bref = new Byte(b1);
+		System.out.println(bref);
+		char ch = 'A';
+		Character cref = new Character(ch);
+		System.out.println(cref);
+
+	}
+}
