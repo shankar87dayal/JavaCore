fc3e654cbf3dd055f7cd1d23e58569e38d617415
User <User@DESKTOP-98P96A5>
2022-03-24 10:55:26 +0530

    Lab1176  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1176.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+
+public class Lab1176 {
+	public static void main(String[] args) {
+//	System.out.println(Boolean.MIN_VALUE);
+//	System.out.println(Boolean.MAX_VALUE);
+//	System.out.println(Boolean.SIZE);
+		System.out.println(Boolean.FALSE);
+		System.out.println(Boolean.TRUE);
+	}
+}
