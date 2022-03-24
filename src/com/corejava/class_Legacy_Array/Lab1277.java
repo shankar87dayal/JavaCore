64238a2b14ac82fd952c9590f7e95d869f1d30b7
User <User@DESKTOP-98P96A5>
2022-03-24 11:31:43 +0530

    Class Lab1214 problem of Array and Legacy class

---
+++ src/com/corejava/class_Legacy_Array/Lab1277.java
@@ -0,0 +1,13 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.Vector;
+
+public class Lab1277 {
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		v.addElement("Sri");
+		v.addElement("vas");
+		v.addElement("Dande");
+		System.out.println(v);
+	}
+}
