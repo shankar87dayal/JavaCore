0008d30f5610d94a5a24a623dd9959d5378fce9f
User <User@DESKTOP-98P96A5>
2022-03-24 11:01:54 +0530

    Lab1188  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1188.java
@@ -0,0 +1,10 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1188 {
+	public static void main(String[] args) {
+		Long val = 123;
+		Double d1 = 12.23F;
+		System.out.println(val);
+		System.out.println(d1);
+	}
+}
