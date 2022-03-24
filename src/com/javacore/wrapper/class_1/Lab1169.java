4c877f2fbb0f15def04ed942749bc63c35319435
User <User@DESKTOP-98P96A5>
2022-03-24 10:52:45 +0530

    Lab1169  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1169.java
@@ -0,0 +1,11 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1169 {
+	public static void main(String[] args) {
+		System.out.println(Character.MIN_VALUE);
+		System.out.println(Character.MAX_VALUE);
+		System.out.println((int) Character.MIN_VALUE);
+		System.out.println((int) Character.MAX_VALUE);
+		System.out.println(Character.SIZE);
+	}
+}
