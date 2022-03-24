f46248346635858af87eda024e4dcfc2b37e5b55
User <User@DESKTOP-98P96A5>
2022-03-24 11:56:11 +0530

    Class Lab1296 problem of collection FrameWork List and their subclass

---
+++ src/com/corejava/class_Legacy_Array/Lab1296.java
@@ -0,0 +1,13 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.*;
+
+public class Lab1296 {
+public static void main(String[] args) {
+	Vector v = new Vector();
+	Enumeration enu = v.elements();
+	System.out.println(enu.nextElement());
+	System.out.println("main eompleted");
+
+}
+}
