a8f426cf59a26a63b82a5d432d0e85bf2c73f488
User <User@DESKTOP-98P96A5>
2022-03-24 10:58:49 +0530

    Lab1182  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1182.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1182 {
+	public static void main(String[] args) {
+		Character ch1 = 'A';
+		Character ch2 = 'A';
+		System.out.println(ch1 == ch2);
+		Character ch3 = (char) 171;
+		Character ch4 = (char) 171;
+		System.out.println(ch3 == ch4);
+	}
+}
