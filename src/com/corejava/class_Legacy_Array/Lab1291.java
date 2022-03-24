b54269aba34ba8e96a80b365ad77a18b0a8aeef6
User <User@DESKTOP-98P96A5>
2022-03-24 11:42:56 +0530

    Class Lab1291 problem of collection FrameWork List

---
+++ src/com/corejava/class_Legacy_Array/Lab1291.java
@@ -0,0 +1,26 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Lab1291 {
+	public static void main(String[] args) {
+		Collection col1 = new ArrayList();
+		col1.add("JDBC");
+		col1.add("sri");
+		col1.add("jsp");
+		col1.add("sirnivas");
+		col1.add("java");
+		col1.add("Dende");
+		Collection col2 = new ArrayList();
+		col2.add("java");
+		col2.add("JDBC");
+		col2.add("JSP");
+		System.out.println(col1);
+		System.out.println(col2);
+		System.out.println(col1.retainAll(col2));
+		System.out.println(col1);
+		System.out.println(col2);
+	}
+
+}
