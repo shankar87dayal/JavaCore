0a356b676d2d0cee223a001b0ff3621a6123ff4c
User <User@DESKTOP-98P96A5>
2022-03-24 11:13:10 +0530

    Lab1199  problem of system class

---
+++ src/com/corejava/classSystem/Lab1199.java
@@ -0,0 +1,9 @@
+package com.corejava.classSystem;
+
+public class Lab1199 {
+	public static void main(String[] args) {
+		int arr1[] = { 40, 50, 60, 70 };
+		String arr3[] = new String[5];
+		System.arraycopy(arr1, 0, arr3, 0, arr1.length);
+	}
+}
