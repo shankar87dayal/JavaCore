27d1abbc13c9e319ab0e762bd0855b0be78fc151
User <User@DESKTOP-98P96A5>
2022-03-24 11:16:32 +0530

    Lab1201  problem of Math class

---
+++ src/com/corejava/classRuntime/Lab1203.java
@@ -0,0 +1,23 @@
+package com.corejava.classRuntime;
+
+public class Lab1203 {
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		System.out.println(Math.PI);
+		System.out.println(Math.sqrt(16));
+		System.out.println(Math.pow(3, 2));
+		System.out.println(Math.abs(-99));
+		System.out.println(Math.max(20, 10));
+		System.out.println(Math.max(99.9, 20.5));
+		System.out.println(Math.min(99.9, 20.5));
+		System.out.println(Math.ceil(10.2));
+		System.out.println(Math.ceil(10.5));
+		System.out.println(Math.ceil(10.9));
+		System.out.println(Math.floor(10.2));
+		System.out.println(Math.floor(10.5));
+		System.out.println(Math.floor(10.9));
+		System.out.println(Math.round(10.2));
+		System.out.println(Math.round(10.5));
+		System.out.println(Math.round(10.9));
+	}
+}
