7b0187362b7b4a0ef31229ac70ab16a58439a03b
User <User@DESKTOP-98P96A5>
2022-03-24 11:01:36 +0530

    Lab1187  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1187.java
@@ -0,0 +1,10 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1187 {
+	public static void main(String[] args) {
+		Object obj = 123;
+		Number num = 123L;
+		System.out.println(obj);
+		System.out.println(num);
+	}
+}
