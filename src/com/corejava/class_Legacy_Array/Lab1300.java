2d060f899caca3e3a554e09da7908d8ac860f156
User <User@DESKTOP-98P96A5>
2022-03-24 11:59:47 +0530

    Class Lab1300 problem of collection FrameWork List interface

---
+++ src/com/corejava/class_Legacy_Array/Lab1300.java
@@ -0,0 +1,13 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.*;
+
+public class Lab1300 {
+public static void main(String[] args) {
+	List list= new ArrayList();
+	Iterator it = list.iterator();
+	if(it.hasNext())
+	System.out.println(it.next());
+	System.out.println("main completed");
+}
+}
