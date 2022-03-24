45c279d673e1625d8f6e95cb4ed740162218dbd7
User <User@DESKTOP-98P96A5>
2022-03-24 10:50:45 +0530

    Lab1165  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1165.java
@@ -0,0 +1,10 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1165 {
+	public static void main(String[] args) {
+//		System.out.println(Double.parseDouble("101",2));
+//		System.out.println(Double.parseDouble("101",8));
+//		System.out.println(Double.valueOf("101",2));
+//		System.out.println(Double.valueOf("101",16));
+		}
+}
