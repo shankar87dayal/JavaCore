319f5fe406c3c0f545baa6ec20281a876e93b71d
User <User@DESKTOP-98P96A5>
2022-03-24 10:53:11 +0530

    Lab1170  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1170.java
@@ -0,0 +1,17 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1170 {
+	public static void main(String[] args) {
+		System.out.println(Character.isLowerCase('a'));
+		System.out.println(Character.isLowerCase('A'));
+		System.out.println(Character.isUpperCase('B'));
+		System.out.println(Character.isUpperCase('b'));
+		System.out.println(Character.isDigit('9'));
+		System.out.println(Character.isDigit('q'));
+		System.out.println(Character.isLetter('j'));
+		System.out.println(Character.isLetter('9'));
+		System.out.println(Character.isLetterOrDigit('9'));
+		System.out.println(Character.isLetterOrDigit('J'));
+		System.out.println(Character.isLetterOrDigit('$'));
+	}
+}
