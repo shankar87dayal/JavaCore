8e3ce12b7a1462836310452c5758252ce913b392
User <User@DESKTOP-98P96A5>
2022-03-24 10:50:10 +0530

    Lab1163  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1163.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1163 {
+	public static void main(String[] args) {
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.SIZE);
+		System.out.println(Double.POSITIVE_INFINITY);
+		System.out.println(Double.NEGATIVE_INFINITY);
+		System.out.println(Double.NaN);
+	}
+}
