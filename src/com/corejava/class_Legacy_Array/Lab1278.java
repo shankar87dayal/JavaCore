8e28f97834e59e69d6318ffc160987d13a4602ae
User <User@DESKTOP-98P96A5>
2022-03-24 11:32:38 +0530

    Class Lab1278 problem of Array and Legacy class

---
+++ src/com/corejava/class_Legacy_Array/Lab1278.java
@@ -0,0 +1,13 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.Properties;
+
+public class Lab1278 {
+	public static void main(String[] args) {
+		Properties p = new Properties();
+		p.put("jlc-99", "Sri");
+		p.put("jlc-88", "vas");
+		p.put("jlc-77", "Dande");
+		System.out.println(p);
+	}
+}
