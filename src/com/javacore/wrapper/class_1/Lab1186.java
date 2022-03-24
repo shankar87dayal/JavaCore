22d7e18de4d8012faf006c21ac40eb765250533c
User <User@DESKTOP-98P96A5>
2022-03-24 11:00:36 +0530

    Lab1186  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1186.java
@@ -0,0 +1,11 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1186 {
+	public static void main(String[] args) {
+		Integer in1 = 123;
+		Integer in2 = 124;
+		System.out.println(in1 == in2);
+		in1++;
+		System.out.println(in1 == in2);
+	}
+}
