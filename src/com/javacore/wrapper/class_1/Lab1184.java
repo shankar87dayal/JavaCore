e9f401e890cfc581bb9c1cf8250ba28a29561364
User <User@DESKTOP-98P96A5>
2022-03-24 10:59:37 +0530

    Lab1184  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1184.java
@@ -0,0 +1,13 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1184 {
+	public static void main(String[] args) {
+		Float f1 = 12.23F;
+		Float f2 = 12.23F;
+		System.out.println(f1 == f2);
+
+		Double d1 = 12.23;
+		Double d2 = 12.23;
+		System.out.println(d1 == d2);
+	}
+}
