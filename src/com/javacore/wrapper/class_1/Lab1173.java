1a5d70bfe52c29f8636c4b9abacb262cf982176f
User <User@DESKTOP-98P96A5>
2022-03-24 10:54:09 +0530

    Lab1173  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1173.java
@@ -0,0 +1,14 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1173 {
+	public static void main(String[] args) {
+		System.out.println(Character.toLowerCase('A'));
+		System.out.println(Character.toLowerCase('a'));
+		System.out.println(Character.toLowerCase('3'));
+		System.out.println(Character.toLowerCase(66));
+		System.out.println(Character.toUpperCase('a'));
+		System.out.println(Character.toUpperCase('A'));
+		System.out.println(Character.toUpperCase('3'));
+		System.out.println(Character.toUpperCase(98));
+	}
+}
