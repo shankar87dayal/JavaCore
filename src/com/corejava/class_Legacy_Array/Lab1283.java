e4b75dadce3d1295bd588125d112f196c529b746
User <User@DESKTOP-98P96A5>
2022-03-24 11:36:33 +0530

    Class Lab1280 problem of collection FrameWork List

---
+++ src/com/corejava/class_Legacy_Array/Lab1283.java
@@ -0,0 +1,21 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.*;
+
+public class Lab1283 {
+	public static void main(String[] args) {
+		Collection col = new ArrayList();
+		System.out.println(col);
+		System.out.println("size: " + col.size());
+		System.out.println("Empty: " + col.isEmpty());
+		System.out.println();
+		col.add("Sri");
+		col.add("Nivas");
+		col.add("Srini");
+		col.add("SD");
+		col.add("Dende");
+		System.out.println(col);
+		System.out.println("size: " + col.size());
+		System.out.println("Empty: " + col.isEmpty());
+	}
+}
