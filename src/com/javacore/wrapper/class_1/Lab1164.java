6e46edde878de802c82eca3572cc33b838896848
User <User@DESKTOP-98P96A5>
2022-03-24 10:50:26 +0530

    Lab1164  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1164.java
@@ -0,0 +1,11 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1164 {
+	public static void main(String[] args) {
+//		String str = Double.toBinaryString(65.0);
+//		System.out.println(Double.toOctalString(65D));
+			System.out.println(Double.toHexString(16.0));
+			System.out.println(Double.toHexString(32.1D));
+			System.out.println(Double.toHexString(0.0));
+		}
+}
