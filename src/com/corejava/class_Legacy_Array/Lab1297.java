252e1dbc83c65d57ae6a548dcfbf0fa68a24a52a
User <User@DESKTOP-98P96A5>
2022-03-24 11:57:25 +0530

    Class Lab1297 problem of collection FrameWork List interface

---
+++ src/com/corejava/class_Legacy_Array/Lab1297.java
@@ -0,0 +1,14 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.*;
+
+public class Lab1297 {
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		Enumeration enu = v.elements();
+		if(enu.hasMoreElements())
+		System.out.println(enu.nextElement());
+		System.out.println("main eompleted");
+
+	}
+}
