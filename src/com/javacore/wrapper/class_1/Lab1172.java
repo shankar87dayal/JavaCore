1b4c6dc8f5887588bc9246963a2becb0e12bfdbd
User <User@DESKTOP-98P96A5>
2022-03-24 10:53:49 +0530

    Lab1172  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1172.java
@@ -0,0 +1,21 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1172 {
+	public static void main(String[] args) {
+		System.out.println(Character.isAlphabetic(65));
+		System.out.println(Character.isAlphabetic(97));
+		System.out.println(Character.isAlphabetic(48));
+		System.out.println(Character.isSpaceChar(' '));
+		System.out.println(Character.isSpace('  '));
+		System.out.println(Character.isSpace('\n'));
+		System.out.println(Character.isSpace('\f'));
+		System.out.println(Character.isSpace('\r'));
+		System.out.println(Character.isSpace('A'));
+		System.out.println(Character.isMirrored(']'));
+		System.out.println(Character.isMirrored('('));
+		System.out.println(Character.isMirrored('}'));
+		System.out.println(Character.isMirrored('W'));
+		System.out.println(Character.isMirrored(65));
+
+	}
+}
