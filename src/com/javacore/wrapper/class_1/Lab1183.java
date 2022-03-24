a4374a0582d786a17ab8d29a22b55ccd9f94cde0
User <User@DESKTOP-98P96A5>
2022-03-24 10:59:10 +0530

    Lab1183  problem of wrapper class

---
+++ src/com/javacore/wrapper/class_1/Lab1183.java
@@ -0,0 +1,12 @@
+package com.javacore.wrapper.class_1;
+
+public class Lab1183 {
+	public static void main(String[] args) {
+		Long val1 = 127L;
+		Long val2 = 127L;
+		System.out.println(val1 == val2);
+		Long val3 = 128L;
+		Long val4 = 128L;
+		System.out.println(val3 == val4);
+	}
+}
