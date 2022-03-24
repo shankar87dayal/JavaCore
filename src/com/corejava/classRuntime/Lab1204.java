10c568e9211cb97587e6588b936d655e127d2acd
User <User@DESKTOP-98P96A5>
2022-03-24 11:16:56 +0530

    Lab1204  problem of Math class

---
+++ src/com/corejava/classRuntime/Lab1204.java
@@ -0,0 +1,13 @@
+package com.corejava.classRuntime;
+
+public class Lab1204 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(Math.random());
+		}
+		System.out.println("\n*****");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(Math.random());
+		}
+	}
+}
