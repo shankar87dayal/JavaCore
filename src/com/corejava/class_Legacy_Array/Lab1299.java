1954685229dc2fd568b3310de7c2f4c5b2e83fd5
User <User@DESKTOP-98P96A5>
2022-03-24 11:59:14 +0530

    Class Lab1299 problem of collection FrameWork List interface

---
+++ src/com/corejava/class_Legacy_Array/Lab1299.java
@@ -0,0 +1,11 @@
+package com.corejava.class_Legacy_Array;
+
+import java.util.*;
+public class Lab1299 {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		Iterator it = list.iterator();
+		System.out.println(it.next());
+		System.out.println("main completed");
+	}
+}
