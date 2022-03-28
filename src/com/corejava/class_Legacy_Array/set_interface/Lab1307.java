6fa1ec1ece9988b4ae19d907d505c74147654475
User <User@DESKTOP-98P96A5>
2022-03-28 20:54:00 +0530

    Class Lab1307 problem of collection FrameWork set interface

---
+++ src/com/corejava/class_Legacy_Array/set_interface/Lab1307.java
@@ -0,0 +1,15 @@
+package com.corejava.class_Legacy_Array.set_interface;
+
+import java.util.HashSet;
+
+public class Lab1307 {
+	public static void main(String[] args) {
+		HashSet set = new HashSet();
+		set.add("Sir");
+		set.add("Nivas");
+		set.add("99");
+		set.add("Dande");
+		set.add("JLCINDIA");
+		System.out.println(set);
+	}
+}
