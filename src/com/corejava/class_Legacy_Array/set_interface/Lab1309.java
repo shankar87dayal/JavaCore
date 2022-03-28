db8ea69a0d1085e8473de7f58ec1a6d0a0179bf0
User <User@DESKTOP-98P96A5>
2022-03-28 20:55:04 +0530

    Class Lab1309 problem of collection FrameWork set interface

---
+++ src/com/corejava/class_Legacy_Array/set_interface/Lab1309.java
@@ -0,0 +1,16 @@
+package com.corejava.class_Legacy_Array.set_interface;
+
+import java.util.*;
+
+public class Lab1309 {
+	public static void main(String[] args) {
+		TreeSet set = new TreeSet();
+		set.add("Sir");
+		set.add("Nivas");
+		set.add("99");
+		set.add("Dande");
+		set.add("JLCINDIA");
+		System.out.println(set);
+	}
+}
+
