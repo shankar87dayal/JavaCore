b2983153f340fbec4b47085a15948ddf75b63fdc
User <User@DESKTOP-98P96A5>
2022-04-03 10:26:09 +0530

    Class Lab1419 problem of Input/ Output

---
+++ src/com/corejava/class_I_O/Lab1419.java
@@ -0,0 +1,12 @@
+package com.corejava.class_I_O;
+
+import java.io.File;
+
+public class Lab1419 {
+	public static void main(String[] args) {
+		System.out.println("File.path.separator\t:" + File.pathSeparator);
+		System.out.println("File.path.separatorChar\t:" + File.pathSeparatorChar);
+		System.out.println("File.separator\t:" + File.separator);
+		System.out.println("File.path.separatorChar\t:" + File.separatorChar);
+	}
+}
