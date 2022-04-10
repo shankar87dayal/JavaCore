209b915d6e884d1f1197c3b24c484cf34ec79b4e
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-10 10:15:24 +0530

    class Lab1370 problem of threads

---
+++ src/com/corejava/class_Class_threads/Lab1370.java
@@ -0,0 +1,33 @@
+package com.corejava.class_Class_threads;
+
+public class Lab1370 {
+	public static void main(String[] args) {
+		MyThread t1 = new MyThread();
+		MyThread t2 = new MyThread();
+		t1.start();
+		t2.start();
+		Thread t = Thread.currentThread();
+		for (int i = 100; i <= 110; i++) {
+			System.out.println(t.getName() + " -value is:-" + i);
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class MyThread extends Thread {
+	public void run() {
+		Thread th = Thread.currentThread();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(th.getName() + " -value is:-" + i);
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
