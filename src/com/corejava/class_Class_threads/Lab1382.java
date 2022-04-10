a69328798896fd42616895818a0ea45cecc31484
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-10 10:19:52 +0530

    class Lab1382 problem of threads

---
+++ src/com/corejava/class_Class_threads/Lab1382.java
@@ -0,0 +1,13 @@
+package com.corejava.class_Class_threads;
+
+public class Lab1382 {
+	public static void main(String[] args) {
+		MyThread_3 t1 = new MyThread_3();
+		t1.setName("raushan");
+		System.out.println(t1);
+	}
+}
+
+class MyThread_3 extends Thread {
+
+}
